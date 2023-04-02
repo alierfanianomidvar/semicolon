3e98dfcb1cbce509d3e3fd430c29f6a396f3cd82
hamideh <haeri.hamideh@gmail.com>
2023-04-02 12:20:50 +0200

    Add Pharmacy and TimeTable entities Add PharmacyRepository

---
+++ src/main/java/com/unipd/semicolon/core/entity/TimeTable.java
@@ -0,0 +1,50 @@
+package com.unipd.semicolon.core.entity;
+
+public class TimeTable
+{
+    int dayOfWeek;
+    String fromHour;
+    String toHour;
+
+    public TimeTable() {
+    }
+
+    public TimeTable(int dayOfWeek, String fromHour, String toHour) {
+        this.dayOfWeek = dayOfWeek;
+        this.fromHour = fromHour;
+        this.toHour = toHour;
+    }
+
+    public int getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(int dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public String getFromHour() {
+        return fromHour;
+    }
+
+    public void setFromHour(String fromHour) {
+        this.fromHour = fromHour;
+    }
+
+    public String getToHour() {
+        return toHour;
+    }
+
+    public void setToHour(String toHour) {
+        this.toHour = toHour;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeTable{" +
+                "dayOfWeek=" + dayOfWeek +
+                ", fromHour='" + fromHour + '\'' +
+                ", toHour='" + toHour + '\'' +
+                '}';
+    }
+}
