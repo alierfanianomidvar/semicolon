273b2cbc8dbf99b55e5de35d14077786954100c6
Ali Tahvildari <ali.tahvildari@studenti.unipd.it>
2023-04-03 16:20:47 +0200

    Code has been modified due to the errors methods have been deleted

--- src/main/java/com/unipd/semicolon/core/entity/Order.java
+++ src/main/java/com/unipd/semicolon/core/entity/Order.java
@@ -1,127 +1,58 @@
 package com.unipd.semicolon.core.entity;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
 public class Order {
-    // order Id
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Map of order items with drug id as key and amount as value
-    private Map<Long, Integer> orderItems;
+    @Column(name = "order_date")
+    private LocalDate orderDate;
 
-    // Total price of the order
-    private BigDecimal totalPrice;
-
-    // Date when the order was placed
-    private LocalDate date;
-
-    // Status of the order (true for processed, false for pending)
     private boolean status;
 
-    // Constructor
-    public Order(Long id, Map<Long, Integer> orderItems, BigDecimal totalPrice, LocalDate date, boolean status) {
-        this.id = id;
-        this.orderItems = orderItems;
-        this.totalPrice = totalPrice;
-        this.date = date;
-        this.status = status;
-    }
+    private double totalPrice;
 
-    // Getters and setters
+    // Getters
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Map<Long, Integer> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setOrderItems(Map<Long, Integer> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-    public BigDecimal getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(BigDecimal totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public LocalDate getOrderDate() {
+        return orderDate;
     }
 
     public boolean isStatus() {
         return status;
     }
 
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+
+    // Setters
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
     public void setStatus(boolean status) {
         this.status = status;
     }
 
-    // Helper methods
-
-    // Add an order item to the map
-    public void addItem(Long drugId, int amount) {
-        if (orderItems == null) {
-            orderItems = new HashMap<>();
-        }
-        if (orderItems.containsKey(drugId)) {
-            int currentAmount = orderItems.get(drugId);
-            orderItems.put(drugId, currentAmount + amount);
-        } else {
-            orderItems.put(drugId, amount);
-        }
-    }
-
-    // Remove an order item from the map
-    public void removeItem(Long drugId) {
-        if (orderItems != null) {
-            orderItems.remove(drugId);
-        }
-    }
-
-    // Calculate the total price of the order based on the drug prices
-    public void calculateTotalPrice(Map<Long, BigDecimal> drugPrices) {
-        BigDecimal total = BigDecimal.ZERO;
-        if (orderItems != null && drugPrices != null) {
-            for (Map.Entry<Long, Integer> entry : orderItems.entrySet()) {
-                Long drugId = entry.getKey();
-                Integer amount = entry.getValue();
-                BigDecimal price = drugPrices.get(drugId);
-                if (price != null) {
-                    total = total.add(price.multiply(BigDecimal.valueOf(amount)));
-                }
-            }
-        }
-        totalPrice = total;
-    }
-
-    // Change status function boolean "True" and "False"
-    public void changeStatus() {
-        status = !status;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", orderItems=" + orderItems +
-                ", totalPrice=" + totalPrice +
-                ", date=" + date +
-                ", status=" + status +
-                '}';
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
     }
 }
