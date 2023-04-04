caaa0af6a339e2a513503546b9b348dab83c7e5e
Mohammad Torabi <mohammad.torabi@studenti.unipd.it>
2023-04-04 16:17:05 +0200

    Adding @Entity; Adding Table with columns; fixing constructor's parameters in multiple lines; Removing Methods; Removing DrugEntry inner Class

--- src/main/java/com/unipd/semicolon/core/entity/Receipt.java
+++ src/main/java/com/unipd/semicolon/core/entity/Receipt.java
@@ -1,144 +1,91 @@
 package com.unipd.semicolon.core.entity;
 
+import java.util.Date;
+import java.util.List;
 
-import java.util.*;
+import jakarta.persistence.*;
+@Entity
+@Table(name = "receipt")
 public class Receipt {
 
-        private int id;
-        private List<DrugEntry> drugs;
-        private byte[] image;
-        private Date date;
-        private PaymentMethod paymentMethod;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-        public Receipt(int id, List<DrugEntry> drugs, byte[] image, Date date, PaymentMethod paymentMethod) {
-            this.id = id;
-            this.drugs = drugs;
-            this.image = image;
-            this.date = date;
-            this.paymentMethod = paymentMethod;
-        }
+    @Column(name = "drug_list")
+    private String drug_list; //temp code just to check if it runs or not
+    //upcoming code: private List<Drug> drug_list;
+    //Drug to be connected later
+    //
 
-        // getters and setters for all attributes
+    @Column(name = "image")
+    private byte[] image;
 
-        public int getId() {
-            return id;
-        }
+    @Column(name = "date")
+    private Date date;
 
-        public void setId(int id) {
-            this.id = id;
-        }
+    @Column(name = "payment_method")
+    private PaymentMethod paymentMethod;
 
-        public List<DrugEntry> getDrugs() {
-            return drugs;
-        }
+    public Receipt() {}
 
-        public void setDrugs(List<DrugEntry> drugs) {
-            this.drugs = drugs;
-        }
+    public Receipt(String drug_list, // temp code. upcoming code:List<> drug_list
+                   byte[] image,
+                   Date date,
+                   PaymentMethod paymentMethod) {
+        this.drug_list = drug_list;
+        this.image = image;
+        this.date = date;
+        this.paymentMethod = paymentMethod;
+    }
 
-        public byte[] getImage() {
-            return image;
-        }
+    // getters and setters for all attributes
 
-        public void setImage(byte[] image) {
-            this.image = image;
-        }
+    public int getId() {
+        return id;
+    }
 
-        public Date getDate() {
-            return date;
-        }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        public void setDate(Date date) {
-            this.date = date;
-        }
+    public String getDrug_list() { //temp code. upcoming code: List<Drug> getDrug_list()
+        return drug_list;
+    }
 
-        public PaymentMethod getPaymentMethod() {
-            return paymentMethod;
-        }
+    public void setDrug_list(String drug_list) {//temp code. upcoming code: setDrug_list(List<Drug> drug_list)
+        this.drug_list = drug_list;
+    }
 
-        public void setPaymentMethod(PaymentMethod paymentMethod) {
-            this.paymentMethod = paymentMethod;
-        }
+    public byte[] getImage() {
+        return image;
+    }
 
-        // method to calculate the total cost of the drugs on the receipt
-        public double getTotalCost() {
-            double total = 0;
-            for (DrugEntry entry : drugs) {
-                total += entry.getAmount() * entry.getPrice();
-            }
-            return total;
-        }
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 
-        // inner class to represent an entry for a drug on the receipt
-        public static class DrugEntry {
-            private int drugId;
-            private int amount;
-            private double price;
+    public Date getDate() {
+        return date;
+    }
 
-            public DrugEntry(int drugId, int amount, double price) {
-                this.drugId = drugId;
-                this.amount = amount;
-                this.price = price;
-            }
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-            // getters and setters for all attributes
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
 
-            public int getDrugId() {
-                return drugId;
-            }
-
-            public void setDrugId(int drugId) {
-                this.drugId = drugId;
-            }
-
-            public int getAmount() {
-                return amount;
-            }
-
-            public void setAmount(int amount) {
-                this.amount = amount;
-            }
-
-            public double getPrice() {
-                return price;
-            }
-
-            public void setPrice(double price) {
-                this.price = price;
-            }
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
     }
 
     // enum to represent the payment methods available
-    enum PaymentMethod {
+    public enum PaymentMethod {
         CASH,
         CREDIT_CARD,
         DEBIT_CARD,
         PAYPAL
-    }
-
-    // method to create a new receipt
-    public static Receipt create(int id, List<DrugEntry> drugs, byte[] image, Date date, PaymentMethod paymentMethod) {
-        return new Receipt(id, drugs, image, date, paymentMethod);
-    }
-
-    // method to get the history of all receipts
-    public static List<Receipt> history(List<Receipt> receipts) {
-        return receipts;
-    }
-
-    // method to get the prescription for a specific drug on the receipt
-    public int getPrescription(int drugId) {
-        // logic to get the prescription for a specific drug
-        return 0;
-    }
-
-    // method to edit the date (deadline) for the receipt
-    public void editDate(Date newDate) {
-        this.date = newDate;
-    }
-
-    // method to approve the receipt
-    public void approve() {
-        // logic to approve the receipt
     }
 }
