80c7bba563123d3a8a093572f786acad802750f2
Irem Goksu Celik <iremgoksu.celik@studenti.unipd.it>
2023-04-05 11:04:24 +0200

    Adding @Entity and @Table, Fixing the method and cleaning the toString part for later updates, Adding Repository

--- src/main/java/com/unipd/semicolon/core/entity/Login.java
+++ src/main/java/com/unipd/semicolon/core/entity/Login.java
@@ -1,20 +1,28 @@
 package com.unipd.semicolon.core.entity;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "login")
 public class Login {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String userId;
+    @Column(name = "userName")
     private String userName;
+    @Column(name = "password")
     private String password;
+    @Column(name = "session")
     private boolean session;
+    @Column(name = "lastLoginDate")
     private LocalDateTime lastLoginDate;
+    @Column(name = "activationDate")
     private LocalDateTime activationDate;
+    @Column(name = "terminationDate")
     private LocalDateTime terminationDate;
+
 
 
     //to get the list of login information
@@ -33,6 +41,10 @@ public class Login {
         this.userId = userId;
         this.userName = userName;
         this.password = password;
+    }
+
+    public Login() {
+
     }
 
     public String getUserId() {
@@ -89,49 +101,6 @@ public class Login {
 
     public void setTerminationDate(LocalDateTime terminationDate) {
         this.terminationDate = terminationDate;
-    }
-
-    public boolean login(Login l) throws Exception {
-        Connection conn = null;
-        PreparedStatement stmnt = null;
-        ResultSet result = null;
-        try {
-            // conn = DataSourceProvider.getDataSource().getConnection();
-            String query = "SELECT * FROM users WHERE userId=? AND userName=? AND password=md5(?);";
-
-            //prepare the statement
-            stmnt = conn.prepareStatement(query);
-            stmnt.setString(1, l.getUserId());
-            stmnt.setString(2, l.getUserName());
-            stmnt.setString(3, l.getPassword());
-
-            //execute the query
-            result = stmnt.executeQuery();
-
-            //handle the results of the query
-            if (result.next()) {
-                return true;
-                //LOGGER.info("Student logged in {}.", student_new.getUserId());
-            }
-            return false;
-            //else {LOGGER.error("error logging in the student {}",student.getUserId());}
-
-        } finally {
-            //closing all the possible resources
-            if (result != null) {
-                result.close();
-            }
-            if (stmnt != null) {
-                stmnt.close();
-            }
-        }
-    }
-
-    public void logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
     }
 
 }
