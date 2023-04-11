9f2e974cb802bd7a0a11b6b63fa73ab6acf26f89
Reihaneh <baghishani.r@gmail.com>
2023-04-11 12:19:29 +0430

    adding base for listing supplier

---
+++ src/main/java/com/unipd/semicolon/base/servlet/ServletRegistrationBean.java
@@ -0,0 +1,14 @@
+/*
+package com.unipd.semicolon.base.servlet;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServletRegistrationBean {
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        return new ServletRegistrationBean(new RestDispatcherServlet(),"/suppliers");
+    }
+}
+*/
