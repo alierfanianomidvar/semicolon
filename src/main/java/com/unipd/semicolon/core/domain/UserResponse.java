66e801df8b47b5c759aef46939aac740d1b33869
hosseinhp1378 <hoseinhp100@gmail.com>
2023-04-13 22:00:15 +0200

    adding user response

--- src/main/java/com/unipd/semicolon/core/domain/UserResponse.java
+++ src/main/java/com/unipd/semicolon/core/domain/UserResponse.java
@@ -1,24 +1,39 @@
 package com.unipd.semicolon.core.domain;
 
+import com.unipd.semicolon.core.entity.Role;
+import com.unipd.semicolon.core.entity.enums.Gender;
+
 import java.util.Date;
 
 public class UserResponse {
     private String name;
     private String familyName;
-    private Date birthday;
-    private String role;
+    private Enum<Gender> gender;
+    private Date birthDate;
+    private String phoneNumber;
+    private String address;
+    private Role role;
+    private String email;
 
     public UserResponse() {
     }
     public UserResponse(
             String name,
             String familyName,
-            Date birthday,
-            String role) {
+            Enum<Gender> gender,
+            Date birthDate,
+            String phoneNumber,
+            String address,
+            Role role,
+            String email) {
         this.name = name;
         this.familyName = familyName;
-        this.birthday = birthday;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
         this.role = role;
+        this.email = email;
     }
 
 
@@ -31,6 +46,46 @@ public class UserResponse {
         this.name = name;
     }
 
+    public Enum<Gender> getGender() {
+        return gender;
+    }
+
+    public void setGender(Enum<Gender> gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getFamilyName() {
         return familyName;
     }
@@ -40,18 +95,18 @@ public class UserResponse {
     }
 
     public Date getBirthday() {
-        return birthday;
+        return birthDate;
     }
 
     public void setBirthday(Date birthday) {
-        this.birthday = birthday;
+        this.birthDate = birthDate;
     }
 
-    public String getRole() {
+    public Role getRole() {
         return role;
     }
 
-    public void setRole(String role) {
+    public void setRole(Role role) {
         this.role = role;
     }
 }
