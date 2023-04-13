927f4d59cc723c2454adc0709ddde000c3e01b03
hosseinhp1378 <hoseinhp100@gmail.com>
2023-04-13 22:00:01 +0200

    adding UserService and its implementation

--- src/main/java/com/unipd/semicolon/business/service/UserService.java
+++ src/main/java/com/unipd/semicolon/business/service/UserService.java
@@ -3,7 +3,9 @@ package com.unipd.semicolon.business.service;
 
 import com.unipd.semicolon.core.domain.UserListExampleResponse;
 import com.unipd.semicolon.core.domain.UserResponse;
+import com.unipd.semicolon.core.entity.Role;
 import com.unipd.semicolon.core.entity.User;
+import com.unipd.semicolon.core.entity.enums.Gender;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -11,24 +13,49 @@ import java.util.List;
 
 public interface UserService {
 
+    //TODO: phone number is Long in Entity part => should change to String
     User save(
             String username,
             String password,
             String name,
-            String familyName,
-            Date birthday,
-            Short role
+            String lastName,
+            Gender gender,
+            Date birthDate,
+            String phoneNumber,
+            String address,
+            Role role,
+            String email,
+            boolean accountStatus,
+            byte[] profilePicture
     );
 
+    //For password retrieval we need to
     Boolean edit(
             Long id,
             String name,
             String familyName,
-            Date birthday,
-            Short role
+            Gender gender,
+            Date birthDate,
+            String phoneNumber,
+            String address,
+            Role role,
+            String email
     );
+
+    //Id => userId
+    Boolean changeStatus(
+            Long Id
+    );
+
 
     List<UserResponse> getAll();
 
-    List<UserListExampleResponse> findAllByFamilyName(String familyName);
+    List<UserResponse> findAllByFamilyName(String familyName);
+
+    List<UserResponse> findAllbyRole(Role role);
+
+    //TODO: User and Login entities must be connected
+    List<UserResponse> findByUserName(User userName);
+
+
 }
