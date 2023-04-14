719d1e93cad454818683e141fd91d109875c0fa8
alierfanianomidvar <ali.erfanianomidvar@egmail.com>
2023-04-14 19:02:35 +0200

    Adding string service.
    encode system using Base64 and bCrypt.

---
+++ src/main/java/com/unipd/semicolon/business/service/Imp/AccountServiceImp.java
@@ -0,0 +1,19 @@
+package com.unipd.semicolon.business.service.Imp;
+
+import com.unipd.semicolon.business.service.AccountService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImp implements AccountService {
+
+
+    @Override
+    public void Login(
+            String username,
+            String password) {
+            // decode pass word .
+            // login repo.
+
+
+    }
+}
