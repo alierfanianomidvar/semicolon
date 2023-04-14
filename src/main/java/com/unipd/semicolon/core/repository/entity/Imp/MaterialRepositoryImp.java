b81bd93db0bda28cd2a894e2b0c512d5ca1af7ee
Ali Tahvildari <ali.tahvildari@studenti.unipd.it>
2023-04-14 14:55:15 +0200

    Adding Mapper and make a correction in repo interface and repo imp

--- src/main/java/com/unipd/semicolon/core/repository/entity/Imp/MaterialRepositoryImp.java
+++ src/main/java/com/unipd/semicolon/core/repository/entity/Imp/MaterialRepositoryImp.java
@@ -9,11 +9,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
-
+@Repository
 public class MaterialRepositoryImp extends CustomRepository implements MaterialRepository {
 
     @Transactional
