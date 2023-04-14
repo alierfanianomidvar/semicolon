6be79317dcd06bf5839d1920e7efba3f22d3f662
Gulce Sirvanci <gulcesirvanci@gmail.com>
2023-04-14 15:26:00 +0300

    Supplier API CRUD

--- src/main/java/com/unipd/semicolon/business/service/Imp/SupplierServiceImp.java
+++ src/main/java/com/unipd/semicolon/business/service/Imp/SupplierServiceImp.java
@@ -34,5 +34,88 @@ public class SupplierServiceImp implements SupplierService {
 
         Supplier supplier = new Supplier(name, address, email, telephoneNumber);
         return supplierDao.create(supplier);
+import com.unipd.semicolon.business.service.SupplierService;
+import com.unipd.semicolon.core.entity.Drug;
+import com.unipd.semicolon.core.entity.Material;
+import com.unipd.semicolon.core.entity.Supplier;
+import com.unipd.semicolon.core.repository.entity.SupplierRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class SupplierServiceImp implements SupplierService {
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    @Override
+    public Object findBySupplierId(Long id) {
+        if(Objects.isNull(supplierRepository.findBySupplierId(id))) {
+            throw new EntityNotFoundException("Supplier Not Found with id" + id);
+        }
+        return supplierRepository.findBySupplierId(id);
+    }
+
+    @Override
+    public Supplier save(String name, String address, String email, String telephoneNumber, List<Drug> drugs, List<Material> materials) throws SQLException {
+        if (name == null || address == null || email == null ||
+                telephoneNumber == null || drugs == null || materials == null ) {
+            throw new IllegalArgumentException("Invalid input parameter");
+        } else {
+            Supplier supplier = new Supplier(
+                    name,
+                    address,
+                    email,
+                    telephoneNumber,
+                    drugs,
+                    materials);
+
+            supplierRepository.addSupplier(supplier);
+            return supplier;
+        }
+    }
+
+    @Override
+    public boolean edit(Long id, String name, String address, String email, String telephoneNumber, List<Drug> drugs, List<Material> materials) throws SQLException {
+        if (Objects.nonNull(supplierRepository.findBySupplierId(id))) {
+            Supplier supplier = supplierRepository.findBySupplierId(id);
+            if (name != null) {
+                supplier.setName(name);
+            }
+            if (address != null) {
+                supplier.setAddress(address);
+            }
+            if (email != null) {
+                supplier.setEmail(email);
+            }
+            if (telephoneNumber != null) {
+                supplier.setTelephoneNumber(telephoneNumber);
+            }
+            if (drugs != null) {
+                supplier.setDrugs(drugs);
+            }
+            if (materials != null) {
+                supplier.setMaterials(materials);
+            }
+            supplierRepository.editSupplier(supplier);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean remove(Long id) throws SQLException {
+        if(Objects.isNull(supplierRepository.findBySupplierId(id))) {
+            throw new IllegalArgumentException("Supplier with this id could not found!");
+        }
+        Supplier supplier = supplierRepository.findBySupplierId(id);
+        supplierRepository.removingSuppliers(supplier);
+        return false;
     }
 }
