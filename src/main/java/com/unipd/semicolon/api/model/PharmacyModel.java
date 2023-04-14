c936c30af2fe6c282bd23ed18d5a8e2ec69586a4
Reihaneh <baghishani.r@gmail.com>
2023-04-14 12:31:50 +0430

    adding logic and model for change pharmacy status

--- src/main/java/com/unipd/semicolon/api/model/PharmacyModel.java
+++ src/main/java/com/unipd/semicolon/api/model/PharmacyModel.java
@@ -1,4 +1,16 @@
 package com.unipd.semicolon.api.model;
 
+import com.unipd.semicolon.core.entity.enums.PharmacyStatus;
+
 public class PharmacyModel {
+
+    private PharmacyStatus status;
+
+    public PharmacyModel(PharmacyStatus status) {
+        this.status = status;
+    }
+
+    public PharmacyStatus getStatus() {
+        return status;
+    }
 }
