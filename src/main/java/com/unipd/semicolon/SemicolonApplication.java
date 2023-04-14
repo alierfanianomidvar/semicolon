67dfc1f28ff60de68294d7dee729e12fa6af7fd1
alierfanianomidvar <ali.erfanianomidvar@egmail.com>
2023-04-14 02:37:41 +0200

    Adding the dependency and servlet component scan.
    servlet component scan will search for any servlet in project.

--- src/main/java/com/unipd/semicolon/SemicolonApplication.java
+++ src/main/java/com/unipd/semicolon/SemicolonApplication.java
@@ -3,6 +3,7 @@ package com.unipd.semicolon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.unipd.semicolon")
 @EntityScan(basePackages = {"com.unipd.semicolon.core.entity"})
 @EnableJpaRepositories(basePackages = "com.unipd.semicolon.core.repository")
+@ServletComponentScan
 public class SemicolonApplication {
 
     public static void main(String[] args) {
