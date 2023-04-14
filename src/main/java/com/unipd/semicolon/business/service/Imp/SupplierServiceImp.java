91d7fdac22441f87ecd15e570dbfd51fc96e3468
Gulce Sirvanci <gulcesirvanci@gmail.com>
2023-04-14 15:26:00 +0300

    Supplier API CRUD

--- src/main/java/com/unipd/semicolon/business/service/Imp/SupplierServiceImp.java
+++ src/main/java/com/unipd/semicolon/business/service/Imp/SupplierServiceImp.java
@@ -1,6 +1,9 @@
 package com.unipd.semicolon.business.service.Imp;
 
+import com.unipd.semicolon.business.exception.CreatePharmacyDataNotFound;
+import com.unipd.semicolon.business.exception.PharmacyExistsException;
 import com.unipd.semicolon.business.service.SupplierService;
+import com.unipd.semicolon.core.dao.SupplierDao;
 import com.unipd.semicolon.core.entity.Drug;
 import com.unipd.semicolon.core.entity.Material;
 import com.unipd.semicolon.core.entity.Supplier;
@@ -18,6 +21,33 @@ public class SupplierServiceImp implements SupplierService {
 
     @Autowired
     private SupplierRepository supplierRepository;
+
+
+    private SupplierDao supplierDao;
+
+    public SupplierServiceImp() {
+        this.supplierDao = new SupplierDao();
+    }
+
+    @Override
+    public List<Supplier> getSupplierList() throws SQLException {
+        return supplierDao.findAll();
+    }
+
+    @Override
+    public Supplier create(String name, String address, String email, String telephoneNumber) throws SQLException {
+        if (name.isBlank() || address.isBlank() || email.isBlank() || telephoneNumber.isBlank()) {
+            throw new CreatePharmacyDataNotFound();
+        }
+        List<Supplier> suppliers = supplierDao.findByEmail(email);
+        if (suppliers.toArray().length > 0) {
+            throw new PharmacyExistsException();
+        }
+
+        Supplier supplier = new Supplier(name, address, email, telephoneNumber);
+        return supplierDao.create(supplier);
+
+    }
 
     @Override
     public Object findBySupplierId(Long id) {
