1633e6a41d8be4efb87d67c464ab469f338cf128
Reihaneh <baghishani.r@gmail.com>
2023-04-15 00:32:43 +0430

    fixing the logic to handle exception and returning the pharmacy object as a response

--- src/main/java/com/unipd/semicolon/business/service/Imp/PharmacyServiceImp.java
+++ src/main/java/com/unipd/semicolon/business/service/Imp/PharmacyServiceImp.java
@@ -1,10 +1,12 @@
 package com.unipd.semicolon.business.service.Imp;
 
+import com.unipd.semicolon.business.exception.InvalidParameterException;
 import com.unipd.semicolon.business.service.PharmacyService;
 import com.unipd.semicolon.core.entity.Pharmacy;
 import com.unipd.semicolon.core.entity.User;
 import com.unipd.semicolon.core.entity.enums.PharmacyStatus;
 import com.unipd.semicolon.core.repository.entity.PharmacyRepository;
+import com.unipd.semicolon.core.repository.entity.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +17,16 @@ public class PharmacyServiceImp implements PharmacyService {
 
     private final PharmacyRepository pharmacyRepository;
 
+    private final UserRepository userRepository;
+
     @Autowired
-    public PharmacyServiceImp(PharmacyRepository pharmacyRepository) {
+    public PharmacyServiceImp(PharmacyRepository pharmacyRepository, UserRepository userRepository) {
         this.pharmacyRepository = pharmacyRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
-    public Boolean activation(Long pharmacyId, PharmacyStatus status) {
+    public Pharmacy activation(Long pharmacyId, PharmacyStatus status) {
 
         Pharmacy pharmacy = pharmacyRepository.findById(pharmacyId)
                 .orElseThrow(() -> new IllegalStateException("Pharmacy not found - " + pharmacyId));
@@ -32,12 +37,14 @@ public class PharmacyServiceImp implements PharmacyService {
             if (pharmacy.getStaff().toArray().length > 0) {
                 for (User user: pharmacy.getStaff() ) {
                     user.setAccountStatus(status.toString());
+                    userRepository.save(user);
                 }
             }
+            pharmacy =pharmacyRepository.save(pharmacy);
         } else {
-            throw new IllegalStateException("email taken before!");
+            throw new InvalidParameterException();
         }
 
-        return true;
+        return pharmacy;
     }
 }
--- src/main/java/com/unipd/semicolon/business/service/PharmacyService.java
+++ src/main/java/com/unipd/semicolon/business/service/PharmacyService.java
@@ -2,12 +2,13 @@ package com.unipd.semicolon.business.service;
 
 
 
+import com.unipd.semicolon.core.entity.Pharmacy;
 import com.unipd.semicolon.core.entity.enums.PharmacyStatus;
 
 public interface PharmacyService {
 
 
-    Boolean activation(Long id, PharmacyStatus status);
+    Pharmacy activation(Long id, PharmacyStatus status);
 
 
 }
