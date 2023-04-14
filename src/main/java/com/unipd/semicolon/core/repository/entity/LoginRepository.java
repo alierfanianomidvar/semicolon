ca28c8e22f77a2c0343594af00b0f16c7a951781
alierfanianomidvar <ali.erfanianomidvar@egmail.com>
2023-04-14 23:21:46 +0200

    Add login service.

--- src/main/java/com/unipd/semicolon/core/repository/entity/LoginRepository.java
+++ src/main/java/com/unipd/semicolon/core/repository/entity/LoginRepository.java
@@ -2,10 +2,14 @@ package com.unipd.semicolon.core.repository.entity;
 
 import com.unipd.semicolon.core.entity.Login;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.juli.logging.Log;
 
 import java.sql.SQLException;
 
 public interface LoginRepository {
     boolean login(Login l) throws SQLException;
     void logout(HttpServletRequest request);
+
+    Login save(Login login);
+    Login findByUsername(String username);
 }
