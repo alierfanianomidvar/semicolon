150d9c610daa1cff092c5cf66d7757d7b873486a
ali erfanianomidvar <alierfanianomidvar@192.168.1.5>
2023-04-14 23:51:48 +0200

    Adding security service.
    creating token, validate and get the id and role from token.

---
+++ src/main/java/com/unipd/semicolon/business/service/Imp/SecurityServiceImp.java
@@ -0,0 +1,59 @@
+package com.unipd.semicolon.business.service.Imp;
+
+import com.unipd.semicolon.business.service.SecurityService;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class SecurityServiceImp implements SecurityService {
+
+    @Value("${security.jwt.token.secret-key:secret-key}")  // the secret-key
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-sec:36000}")  // Expire time of token : A year.
+    private long validityInMilliseconds;
+
+    @Override
+    public String createToken(String accountId, String role) {
+        Claims claims = Jwts.claims().setSubject(accountId);
+        claims.put("Role" , role);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds * 1000000);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    @Override
+    public boolean validateToken(String token) {
+        return getParseToken(token) != null;
+    }
+
+    @Override
+    public String getRoleFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        return (String) claims.get("role");
+    }
+
+    @Override
+    public String getAccountId(String token) {
+        return (getParseToken(token).getBody().getSubject());
+    }
+
+    /* ----  private class ----- */
+    private Jws<Claims> getParseToken(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+        } catch (JwtException | IllegalArgumentException e) {
+            return null;
+        }
+    }
+}
