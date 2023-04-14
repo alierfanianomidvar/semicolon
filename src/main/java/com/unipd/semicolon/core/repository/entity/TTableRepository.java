83a065667e58fa7ce2ffb6db77cb6d916abb0345
hosseinhp1378 <hoseinhp100@gmail.com>
2023-04-15 00:39:36 +0200

    add repository for TimeTable

---
+++ src/main/java/com/unipd/semicolon/core/repository/entity/TTableRepository.java
@@ -0,0 +1,15 @@
+package com.unipd.semicolon.core.repository.entity;
+
+import com.unipd.semicolon.core.entity.TimeTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TTableRepository extends JpaRepository<TimeTable, Long> {
+
+    TimeTable save(TimeTable timeTable);
+
+    Optional<TimeTable> findById(Long id);
+}
