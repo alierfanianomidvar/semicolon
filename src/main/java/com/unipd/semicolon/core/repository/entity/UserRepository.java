69b60de3b86fee134f822b49181269667f11f43b
hosseinhp1378 <hoseinhp100@gmail.com>
2023-04-15 10:38:27 +0200

    add Jparepo for userRepository

--- src/main/java/com/unipd/semicolon/core/repository/entity/UserRepository.java
+++ src/main/java/com/unipd/semicolon/core/repository/entity/UserRepository.java
@@ -3,16 +3,19 @@ package com.unipd.semicolon.core.repository.entity;
 import com.unipd.semicolon.core.entity.Role;
 import com.unipd.semicolon.core.entity.User;
 import io.micrometer.common.lang.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import java.util.List;
 
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User save(User user);
 
     User findUserById(Long id);
+
+    Boolean deleteByPharmacyId(Long id);
 
     List<User> getAll();
 
