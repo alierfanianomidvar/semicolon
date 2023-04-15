f2a9b7e397da855b541559d94e09ccdc4377900b
hosseinhp1378 <hoseinhp100@gmail.com>
2023-04-15 10:45:55 +0200

    add pharmacy repo

--- src/main/java/com/unipd/semicolon/core/repository/entity/PharmacyRepository.java
+++ src/main/java/com/unipd/semicolon/core/repository/entity/PharmacyRepository.java
@@ -1,7 +1,9 @@
 package com.unipd.semicolon.core.repository.entity;
 
 import com.unipd.semicolon.core.entity.Pharmacy;
+import com.unipd.semicolon.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,35 +11,25 @@ import java.util.Optional;
 
 @Repository
 public interface PharmacyRepository
-        extends JpaRepository<Pharmacy, Long> {
+        extends JpaRepository<Pharmacy, Long>, JpaSpecificationExecutor<Pharmacy> {
 
-    // Find Pharmacy by id
-//    Optional<Pharmacy> findById(Long id);
-//
-//    // Save an Pharmacy to the database
-//    Pharmacy save(Pharmacy pharmacy);
-//
-//    // Delete an Pharmacy from the database
+//     Find Pharmacy by id
+    Optional<Pharmacy> findById(Long id);
+
+    // Save an Pharmacy to the database
+    Pharmacy save(Pharmacy pharmacy);
+
+    Pharmacy getPharmacyByName(String name);
+    // Delete an Pharmacy from the database
 //    void delete(Pharmacy pharmacy);
 //
 //    // Get all Pharmacies
 //    List<Pharmacy> findAll();
 //
-//    // Get Pharmacies by name
-//    List<Pharmacy> findByName(String name);
-//
-//    // Get Pharmacies by owner
-//    List<Pharmacy> findByOwner(String owner);           // Its entity should be changed to User
-//
-//    // Get Pharmacies by address
-//    List<Pharmacy> findByAddress(String address);
-//
-//    // Get Pharmacies by tellNumber
-//    List<Pharmacy> findByTellNumber(String tellNumber);
-//
-//    // Get Pharmacies by timeTable (hours)
 //    List<Pharmacy> findByTimeTable(String from, String to);
 //
 //    // Get Pharmacies by timeTable (day)
 //    List<Pharmacy> findByTimeTable(int day);
+
+
 }
