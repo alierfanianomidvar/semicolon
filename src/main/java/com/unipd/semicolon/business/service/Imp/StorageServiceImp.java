077636eb07616002062f69d37f2cf5af5aba8ca4
Mohammad Torabi <mohammad.torabi@studenti.unipd.it>
2023-04-15 15:13:36 +0200

    Fixing StorageServiceImp

--- src/main/java/com/unipd/semicolon/business/service/Imp/StorageServiceImp.java
+++ src/main/java/com/unipd/semicolon/business/service/Imp/StorageServiceImp.java
@@ -11,7 +11,9 @@ import com.unipd.semicolon.core.entity.Storage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import com.unipd.semicolon.core.repository.entity.PharmacyRepository;
 import com.unipd.semicolon.core.repository.entity.StorageRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +21,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StorageServiceImp implements StorageService {
+
     @Autowired
     private StorageRepository storageRepository;
+
+    @Autowired
+    private PharmacyRepository pharmacyRepository;
+
+    //@Autowired
+    //private MaterialRepository materialRepository;
+
+    //@Autowired
+    //private DrugRepository drugRepository;
 
     @Override
     public Storage save(Pharmacy pharmacy,
@@ -28,17 +40,37 @@ public class StorageServiceImp implements StorageService {
                         Material material,
                         int amount,
                         int threshold) {
-        if (pharmacy == null || (drug == null && material == null) || amount < 0 || threshold < 0) {
+        if (pharmacy == null) {
+            throw new IllegalArgumentException("Pharmacy is null");
+        } else if (amount < 0 || threshold < 0) {
             throw new IllegalArgumentException("Invalid input parameter");
+        } else if (drug == null && material == null) {
+            throw new IllegalArgumentException("Either drug or material must be specified");
         } else {
-        Storage storage = new Storage(pharmacy,
-                drug,
-                material,
-                amount,
-                threshold);
+            if (!pharmacyRepository.findById(pharmacy.getId()).isEmpty()) {
+                Pharmacy pharmacyRepositoryById = pharmacyRepository.findById(pharmacy.getId()).get();
+            }
+            //else if (!materialRepository.findById(material.getId()).isEmpty()) {
+             //   Material materialRepositoryById = materialRepository.findById(material.getId()).get();
+              //  Storage storage = new Storage(pharmacyRepositoryById,
+                //        null,
+                 //       materialRepositoryById,
+                  //      amount,
+                   //     threshold);
 
-        storageRepository.save(storage);
-        return storage;}
+            //    return storageRepository.save(storage);
+
+           // } else if (!drugRepository.findById(drug.getId()).isEmpty()) {
+            //    Drug drugRepositoryById = drugRepository.findById(drug.getId()).get();
+             //   Storage storage = new Storage(pharmacyRepositoryById,
+              //          drugRepositoryById,
+              //          null,
+               //         amount,
+               //         threshold);
+              //  return storageRepository.save(storage);
+           // }
+        }
+        return null;
     }
 
     @Override
@@ -48,22 +80,27 @@ public class StorageServiceImp implements StorageService {
                         Material material,
                         int amount,
                         int threshold) {
-        if (id_storage == null || id_storage < 0 || pharmacy == null || (drug == null && material == null) || amount < 0 || threshold < 0) {
-            throw new IllegalArgumentException("Invalid input parameter");
+        if (id_storage == null || id_storage < 0) {
+            throw new IllegalArgumentException("id_storage is null");
         } else {
             Storage storage = storageRepository.findStorageById(id_storage);
             if (storage == null) {
                 throw new NotFoundException();
             }
+            if (pharmacy != null)
                 storage.setPharmacy(pharmacy);
+            if (drug != null)
                 storage.setDrug(drug);
+            if (material != null)
                 storage.setMaterial(material);
+            if (amount > 0)
                 storage.setAmount(amount);
+            if (threshold > 0)
                 storage.setThreshold(threshold);
-                storageRepository.save(storage);
-                return true;
-            }
+            storageRepository.save(storage);
+            return true;
         }
+    }
 
     @Override
     public void delete(Storage storage) {
@@ -80,7 +117,7 @@ public class StorageServiceImp implements StorageService {
         if (storage != null) {
             return storage;
         }
-            throw new EntityNotFoundException("Storage not found with id: " + id);
+        throw new EntityNotFoundException("Storage not found with id: " + id);
     }
 
     @Override
