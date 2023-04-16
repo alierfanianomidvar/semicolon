9ee7c67dd7f67e6892e60dec7f857837221cf530
hamideh <haeri.hamideh@gmail.com>
2023-04-16 08:26:55 +0200

    add reciept api
    it has some issue due to lack of other apis. it must fix

--- src/main/java/com/unipd/semicolon/core/repository/entity/ReceiptRepository.java
+++ src/main/java/com/unipd/semicolon/core/repository/entity/ReceiptRepository.java
@@ -1,9 +1,21 @@
 package com.unipd.semicolon.core.repository.entity;
 
 import com.unipd.semicolon.core.entity.Receipt;
-import com.unipd.semicolon.core.entity.User;
+import com.unipd.semicolon.core.entity.enums.PaymentMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
-public interface ReceiptRepository {
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface ReceiptRepository extends
+        JpaRepository<Receipt, Long>,
+        JpaSpecificationExecutor<Receipt> {
 
     Receipt save(Receipt receipt);
+    Receipt findReceiptById(Long id);
+    List<Receipt> findByPaymentMethod(PaymentMethod paymentMethod);
+    List<Receipt> findByDate(Date date);
 }
