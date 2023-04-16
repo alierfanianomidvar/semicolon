1823fa6c8d3b60bb9cf68625aad5d5d3fd912de7
hamideh <haeri.hamideh@gmail.com>
2023-04-16 08:26:55 +0200

    add reciept api
    it has some issue due to lack of other apis. it must fix

---
+++ src/main/java/com/unipd/semicolon/api/controller/ReceiptController.java
@@ -0,0 +1,64 @@
+package com.unipd.semicolon.api.controller;
+
+import com.unipd.semicolon.api.model.ReceiptModel;
+import com.unipd.semicolon.api.util.helper.ResponseHelper;
+import com.unipd.semicolon.business.service.ReceiptService;
+import com.unipd.semicolon.core.entity.Drug;
+import com.unipd.semicolon.core.entity.Material;
+import com.unipd.semicolon.core.entity.Receipt;
+import com.unipd.semicolon.core.entity.enums.PaymentMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping(value = "/receipt")
+public class ReceiptController {
+
+    @Autowired
+    private ReceiptService receiptService;
+
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public ResponseEntity save(@RequestBody ReceiptModel receipt){
+        return ResponseHelper
+                .response(receiptService.save(
+                        receipt.getList_drug_id(),
+                        receipt.getList_material_id(),
+                        receipt.getImage(),
+                        receipt.getDate(),
+                        receipt.getPaymentMethod()));
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Receipt> getById(@PathVariable Long id) {
+        Receipt receipt = receiptService.getReceiptById(id);
+        return ResponseHelper.response(receipt);
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public ResponseEntity<Void> edit(
+            @RequestBody ReceiptModel receiptModel
+    ) {
+        List<Long> drugIds = receiptModel.getList_drug_id();
+        List<Long> materialIds = receiptModel.getList_material_id();
+        byte[] image = receiptModel.getImage();
+        Date date = receiptModel.getDate();
+        PaymentMethod paymentMethod = receiptModel.getPaymentMethod();
+
+        boolean success = receiptService.edit(receiptModel.getId(), drugIds, materialIds, image, date, paymentMethod);
+        return success ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Receipt>> getAll() {
+        List<Receipt> receipts = receiptService.getList();
+        return ResponseHelper.response(receipts);
+    }
+}
