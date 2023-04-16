eb259e71503534376a61e1f0a6618c3c109417a8
hamideh <haeri.hamideh@gmail.com>
2023-04-16 08:26:55 +0200

    add reciept api
    it has some issue due to lack of other apis. it must fix

---
+++ src/main/java/com/unipd/semicolon/business/service/ReceiptService.java
@@ -0,0 +1,33 @@
+package com.unipd.semicolon.business.service;
+
+import com.unipd.semicolon.core.entity.Drug;
+import com.unipd.semicolon.core.entity.Material;
+import com.unipd.semicolon.core.entity.Receipt;
+import com.unipd.semicolon.core.entity.enums.PaymentMethod;
+
+import javax.ws.rs.core.Response;
+import java.util.Date;
+import java.util.List;
+
+public interface ReceiptService {
+
+    Receipt save(List<Long> drug_id,
+                 List<Long> material_id,
+                 byte[] image,
+                 Date date,
+                 PaymentMethod paymentMethod);
+
+    Boolean edit(Long id,
+                 List<Long> drug_id,
+                 List<Long> material_id,
+                 byte[] image,
+                 Date date,
+                 PaymentMethod paymentMethod);
+
+    List<Receipt> getList();
+    Receipt getReceiptById(Long id);
+
+
+
+
+}
