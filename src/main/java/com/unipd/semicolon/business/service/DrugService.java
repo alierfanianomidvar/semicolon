5a8d7933075c70d6019db3bc377c1407100a81d0
ferdos <ferdos.hajizadehchavari@studenti.unipd.it>
2023-04-16 16:35:48 +0200

    Adding DrugMapper and DrugService

---
+++ src/main/java/com/unipd/semicolon/business/service/DrugService.java
@@ -0,0 +1,52 @@
+package com.unipd.semicolon.business.service;
+
+import com.unipd.semicolon.core.entity.Drug;
+import com.unipd.semicolon.core.entity.Supplier;
+import com.unipd.semicolon.core.entity.enums.AgeGroup;
+import com.unipd.semicolon.core.entity.enums.Country;
+import com.unipd.semicolon.core.entity.enums.Gender;
+
+import java.time.LocalDate;
+
+public interface DrugService {
+    public Drug save(
+            String name,
+            Supplier supplier,
+            LocalDate expirationDate,
+            byte[] image,
+            String shape,
+            Gender gender,
+            AgeGroup ageGroup,
+            boolean isSensitive,
+            boolean needPrescription,
+            String description,
+            int limitation,
+            float price,
+            Country countryOFProduction
+    );
+
+    boolean edit(
+            Long id,
+            String name,
+            Supplier supplier,
+            LocalDate expirationDate,
+            byte[] image,
+            String shape,
+            Gender gender,
+            AgeGroup ageGroup,
+            boolean isSensitive,
+            boolean needPrescription,
+            String description,
+            int limitation,
+            float price,
+            Country countryOFProduction
+    );
+
+    boolean isActive(
+            Drug drug
+    );
+
+    Object getById(Long id);
+
+    Drug grtById(Long id);
+}
