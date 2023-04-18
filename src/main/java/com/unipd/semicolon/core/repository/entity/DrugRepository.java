b9f25dc738edb8099f5a8c44e7f49b067f608d05
ferdos <ferdos.hajizadehchavari@studenti.unipd.it>
2023-04-18 11:17:38 +0200

    Fixing some problem in DrugService and DrugServiceImp and Drug and DrugRepository

--- src/main/java/com/unipd/semicolon/core/repository/entity/DrugRepository.java
+++ src/main/java/com/unipd/semicolon/core/repository/entity/DrugRepository.java
@@ -1,10 +1,12 @@
 package com.unipd.semicolon.core.repository.entity;
 
 import com.unipd.semicolon.core.entity.Drug;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface DrugRepository {
+public interface DrugRepository extends JpaRepository<Drug, Long>, JpaSpecificationExecutor<Drug> {
 
     Drug save(
             Drug drug
