5407a624db8503bc83d592c59914ccfc5b9d9b2f
ferdos <ferdos.hajizadehchavari@studenti.unipd.it>
2023-04-19 22:31:15 +0200

    Fixing  DrugController and DrugModel and DrugMapper

--- src/main/java/com/unipd/semicolon/api/controller/DrugController.java
+++ src/main/java/com/unipd/semicolon/api/controller/DrugController.java
@@ -3,15 +3,17 @@ package com.unipd.semicolon.api.controller;
 
 import com.unipd.semicolon.api.model.DrugModel;
 import com.unipd.semicolon.api.util.helper.ResponseHelper;
-import com.unipd.semicolon.core.entity.Drug;
 import com.unipd.semicolon.business.service.DrugService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
+
+
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping(value = "/Drug")
+@RequestMapping(value = "/drug")
 public class DrugController {
 
     @Autowired
@@ -19,21 +21,25 @@ public class DrugController {
 
     @RequestMapping(value = "/save" , method = RequestMethod.POST)
     public ResponseEntity save(@RequestBody DrugModel model) {
-        return ResponseHelper.response(drugService.save(
-                model.getName(),
-                model.getSupplier(),
-                model.getExpirationDate(),
-                model.getImage(),
-                model.getShape(),
-                model.getGender(),
-                model.getAgeGroup(),
-                model.getIsSensitive(),
-                model.getNeedPrescription(),
-                model.getDescription(),
-                model.getLimitation(),
-                model.getPrice(),
-                model.getCountryOFProduction()
-        ));
+        try {
+            return ResponseHelper.response(drugService.save(
+                    model.getName(),
+                    model.getSupplier(),
+                    model.getExpirationDate(),
+                    model.getImage(),
+                    model.getShape(),
+                    model.getGender(),
+                    model.getAgeGroup(),
+                    model.getIsSensitive(),
+                    model.getNeedPrescription(),
+                    model.getDescription(),
+                    model.getLimitation(),
+                    model.getPrice(),
+                    model.getCountryOFProduction()
+            ));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @RequestMapping(value = "/getById/{id}" , method = RequestMethod.GET)
@@ -43,28 +49,30 @@ public class DrugController {
 
     @RequestMapping(value = "/edit/{id}" , method = RequestMethod.PUT)
     public ResponseEntity edit(@PathVariable("id") Long id , @RequestBody DrugModel model){
-        return ResponseHelper.response(drugService.edit(
-                id,
-                model.getName(),
-                model.getSupplier(),
-                model.getExpirationDate(),
-                model.getImage(),
-                model.getShape(),
-                model.getGender(),
-                model.getAgeGroup(),
-                model.getIsSensitive(),
-                model.getNeedPrescription(),
-                model.getDescription(),
-                model.getLimitation(),
-                model.getPrice(),
-                model.getCountryOFProduction()
-        ));
+        try {
+            return ResponseHelper.response(drugService.edit(
+                    model.getId(),
+                    model.getName(),
+                    model.getSupplier(),
+                    model.getExpirationDate(),
+                    model.getImage(),
+                    model.getShape(),
+                    model.getGender(),
+                    model.getAgeGroup(),
+                    model.getIsSensitive(),
+                    model.getNeedPrescription(),
+                    model.getDescription(),
+                    model.getLimitation(),
+                    model.getPrice(),
+                    model.getCountryOFProduction()
+            ));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @RequestMapping(value = "/isActive/{id}" , method = RequestMethod.GET)
-    public ResponseEntity isActive(@PathVariable("id") Long id){
-        Drug drug = (Drug) drugService.getById(id);
-        return ResponseHelper.response(drugService.isActive(drug));
+    @RequestMapping(value = "/getAll" , method = RequestMethod.GET )
+    public ResponseEntity getAll(){
+        return ResponseHelper.response(drugService.getAll());
     }
-
 }
