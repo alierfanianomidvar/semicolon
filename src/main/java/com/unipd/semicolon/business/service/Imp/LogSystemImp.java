821dfc0a7dfdeddd146872d67a1cdd757e0368cd
ali erfanianomidvar <alierfanianomidvar@192.168.1.19>
2023-04-22 12:31:56 +0200

    Adding the log system files.

---
+++ src/main/java/com/unipd/semicolon/business/service/Imp/LogSystemImp.java
@@ -0,0 +1,8 @@
+package com.unipd.semicolon.business.service.Imp;
+
+import com.unipd.semicolon.business.service.LogSystem;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogSystemImp implements LogSystem {
+}
