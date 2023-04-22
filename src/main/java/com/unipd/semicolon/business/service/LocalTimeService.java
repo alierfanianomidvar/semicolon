415fa83b4ef557acea9589f8f1366bd3ae60b6dc
ali erfanianomidvar <alierfanianomidvar@192.168.1.19>
2023-04-22 22:21:31 +0200

    Adding the local date function.

--- src/main/java/com/unipd/semicolon/business/service/LocalTimeService.java
+++ src/main/java/com/unipd/semicolon/business/service/LocalTimeService.java
@@ -1,10 +1,13 @@
 package com.unipd.semicolon.business.service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public interface LocalTimeService {
 
     LocalDateTime getLocalDateTime();
+
+    LocalDate getLocalDate();
 
     Long nowTime();
 }
