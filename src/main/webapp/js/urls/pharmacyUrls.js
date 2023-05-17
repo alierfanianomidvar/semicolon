3ae255e8ed8f5341c0b173d344e6da4ee3c9df02
ali erfanianomidvar <alierfanianomidvar@192.168.1.32>
2023-05-17 23:10:26 +0200

    Adding the pahrmacy urls

---
+++ src/main/webapp/js/urls/pharmacyUrls.js
@@ -0,0 +1,47 @@
+import BaseApiUrl from './baseUrl';
+
+export default class pharmacyUrls extends BaseApiUrl {
+
+    static PHARMACY_BASE_URL = 'pharmacy';
+
+    static ADD = {
+        url: `${BaseApiUrl.BASE_URL}/${pharmacyUrls.PHARMACY_BASE_URL}`,
+        method: 'POST'
+    };
+
+    static EDIT = {
+        url: `${BaseApiUrl.BASE_URL}/${pharmacyUrls.PHARMACY_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'PUT'
+    };
+
+    static ADD_STAFF = {
+        url: `${BaseApiUrl.BASE_URL}/${pharmacyUrls.PHARMACY_BASE_URL}/add-staff/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'PUT'
+    };
+
+    static DELETE_STAFF = {
+        url: `${BaseApiUrl.BASE_URL}/${pharmacyUrls.PHARMACY_BASE_URL}/delete-staff/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'DELETE',
+    };
+
+    static GET = {
+        url: `${BaseApiUrl.BASE_URL}/${pharmacyUrls.PHARMACY_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'GET'
+    };
+
+    static DELETE = {
+        url: `${BaseApiUrl.BASE_URL}/${pharmacyUrls.PHARMACY_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'DELETE',
+    };
+
+    static GET_ALL = {
+        url: `${BaseApiUrl.BASE_URL}/${pharmacyUrls.PHARMACY_BASE_URL}/get-all`,
+        method: 'GET'
+    };
+
+    static PHARMACY_ACTIVATION = {
+        url: `${BaseApiUrl.BASE_URL}/${pharmacyUrls.PHARMACY_BASE_URL}/get-all`, //NOTE: ADD THIS TO URL /{pharmacyId}
+        method: 'PATCH'
+    };
+
+}
