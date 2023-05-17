6db51a4dce75b0823375f5a93e71111173cac70c
alierfanianomidvar <ali.erfanianomidvar@egmail.com>
2023-05-17 17:31:19 +0200

    Adding the get data.

--- src/main/webapp/js/router.js
+++ src/main/webapp/js/router.js
@@ -12,4 +12,16 @@ class Router {
             });
     }
 
+
+    getData(url, params) {
+        if (params) {
+            url += '?' + Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`).join('&');
+        }
+        this.sendHttpRequest('GET', url)
+            .then(responseData => {
+                console.log("ali")
+                console.log(responseData['data']);
+                return responseData['data'];
+            });
+    }
 }
