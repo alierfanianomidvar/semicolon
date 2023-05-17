b4a0102ccd1f753ef32a360154742254d1765204
alierfanianomidvar <ali.erfanianomidvar@egmail.com>
2023-05-17 23:27:08 +0200

    Adding storage urls.

--- src/main/webapp/js/urls/receiptUrls.js
+++ src/main/webapp/js/urls/receiptUrls.js
@@ -2,21 +2,20 @@ import BaseApiUrl from './baseUrl';
 
 export default class receiptUrls extends BaseApiUrl {
 
-    static PHARMACY_BASE_URL = 'pharmacy';
+    static RECEIPT_BASE_URL = 'receipt';
 
     static ADD = {
-        url: `${BaseApiUrl.BASE_URL}/${receiptUrls.PHARMACY_BASE_URL}`,
+        url: `${BaseApiUrl.BASE_URL}/${receiptUrls.RECEIPT_BASE_URL}`,
         method: 'POST'
     };
 
     static GET_BY_ID = {
-        url: `${BaseApiUrl.BASE_URL}/${receiptUrls.PHARMACY_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        url: `${BaseApiUrl.BASE_URL}/${receiptUrls.RECEIPT_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
         method: 'GET'
     };
 
-
     static EDIT = {
-        url: `${BaseApiUrl.BASE_URL}/${receiptUrls.PHARMACY_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        url: `${BaseApiUrl.BASE_URL}/${receiptUrls.RECEIPT_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
         method: 'PUT'
     };
 
---
+++ src/main/webapp/js/urls/storageUrls.js
@@ -0,0 +1,44 @@
+import BaseApiUrl from './baseUrl';
+
+export default class storageUrls extends BaseApiUrl {
+
+    static STORAGE_BASE_URL = 'storage';
+
+    static ADD = {
+        url: `${BaseApiUrl.BASE_URL}/${storageUrls.STORAGE_BASE_URL}`,
+        method: 'POST'
+    };
+
+    static GET_BY_ID = {
+        url: `${BaseApiUrl.BASE_URL}/${storageUrls.STORAGE_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'POST'
+    };
+
+    static EDIT = {
+        url: `${BaseApiUrl.BASE_URL}/${storageUrls.STORAGE_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'PUT'
+    };
+
+    static DELETE_BY_ID = {
+        url: `${BaseApiUrl.BASE_URL}/${storageUrls.STORAGE_BASE_URL}/`,
+        method: 'DELETE'
+    };
+
+    static GET_ALL = {
+        url: `${BaseApiUrl.BASE_URL}/${storageUrls.STORAGE_BASE_URL}`,
+        method: 'GET',
+    };
+
+    static REPORT = {
+        url: `${BaseApiUrl.BASE_URL}/${storageUrls.STORAGE_BASE_URL}/report/`, //NOTE: ADD THIS TO URL /{pharmacyId}
+        method: 'GET',
+    };
+
+
+    static STATUS = {
+        url: `${BaseApiUrl.BASE_URL}/${storageUrls.STORAGE_BASE_URL}/report`,
+        method: 'GET'
+    };
+
+
+}
