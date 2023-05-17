98bcae0a953c4bd5747e33a22c2245ee74861f31
ali erfanianomidvar <alierfanianomidvar@192.168.1.32>
2023-05-17 22:16:18 +0200

    Adding urls of material.

---
+++ src/main/webapp/js/urls/materialUrls.js
@@ -0,0 +1,31 @@
+import BaseApiUrl from './baseUrl';
+
+export default class materialUrls extends BaseApiUrl {
+
+    static MATERIAL_BASE_URL = 'material';
+
+    static ADD = {
+        url: `${BaseApiUrl.BASE_URL}/${materialUrls.MATERIAL_BASE_URL}`,
+        method: 'POST'
+    };
+
+    static GET_BY_ID = {
+        url: `${BaseApiUrl.BASE_URL}//${materialUrls.MATERIAL_BASE_URL}/`,
+        method: 'POST'
+    };
+
+    static EDIT = {
+        url: `${BaseApiUrl.BASE_URL}/${materialUrls.MATERIAL_BASE_URL}/`,
+        method: 'PUT'
+    };
+
+    static GET_ALL = {
+        url: `${BaseApiUrl.BASE_URL}/${materialUrls.MATERIAL_BASE_URL}`,
+        method: 'GET',
+        queryParameters: {
+            countryOfProduction: '',
+            supplierId: '',
+            gender: ''
+        }
+    };
+}
