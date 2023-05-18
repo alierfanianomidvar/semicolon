10403ba15f829b7b5a8dd0600fb9f912d9f0c445
ali erfanianomidvar <alierfanianomidvar@192.168.1.32>
2023-05-18 09:47:21 +0200

    Adding the user urls.
    Fix method of  get by id for supplier and storage.

--- src/main/webapp/js/urls/supplierUrls.js
+++ src/main/webapp/js/urls/supplierUrls.js
@@ -11,7 +11,7 @@ export default class supplierUrls extends BaseApiUrl {
 
     static GET_BY_ID = {
         url: `${BaseApiUrl.BASE_URL}/${supplierUrls.SUPPLIER_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
-        method: 'POST'
+        method: 'GET'
     };
 
     static EDIT = {
