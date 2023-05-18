1a4a14514f42120508aed171f8ed34cac4c23ae5
ali erfanianomidvar <alierfanianomidvar@192.168.1.32>
2023-05-18 09:37:15 +0200

    Adding supliers urls.

---
+++ src/main/webapp/js/urls/supplierUrls.js
@@ -0,0 +1,33 @@
+import BaseApiUrl from './baseUrl';
+
+export default class supplierUrls extends BaseApiUrl {
+
+    static SUPPLIER_BASE_URL = 'supplier';
+
+    static ADD = {
+        url: `${BaseApiUrl.BASE_URL}/${supplierUrls.SUPPLIER_BASE_URL}`,
+        method: 'POST'
+    };
+
+    static GET_BY_ID = {
+        url: `${BaseApiUrl.BASE_URL}/${supplierUrls.SUPPLIER_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'POST'
+    };
+
+    static EDIT = {
+        url: `${BaseApiUrl.BASE_URL}/${supplierUrls.SUPPLIER_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'PUT'
+    };
+
+    static REMOVE = {
+        url: `${BaseApiUrl.BASE_URL}/${supplierUrls.SUPPLIER_BASE_URL}/`, //NOTE: ADD THIS TO URL /{id}
+        method: 'DELETE'
+    };
+
+    static GET_ALL = {
+        url: `${BaseApiUrl.BASE_URL}/suppliers`,
+        method: 'GET',
+    };
+
+
+}
