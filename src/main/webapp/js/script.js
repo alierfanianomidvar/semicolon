98bfad75e166380fed2b6bc8642c0e0fcbffd8b0
hosseinhp <hossein.hosseinpour@studenti.unipd.it>
2023-05-21 23:08:10 +0200

    added some functionality for handling modal
    Also modified code style

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -5,6 +5,7 @@ import storageUrls from "./urls/storageUrls.js";
 import orderUrls from "./urls/orderUrls.js";
 import supplierUrls from "./urls/supplierUrls.js";
 import userUrls from "./urls/userUrls.js";
+import createModal from "./modal.js";
 $(function () {
 
     // TODO : we must call the api like this example ->
@@ -17,7 +18,7 @@ $(function () {
     let fetchedData = null;
     listItems.forEach(item => {
         item.addEventListener('click', async () => {
-            if(selectedItem) {
+            if (selectedItem) {
                 selectedItem.style.backgroundColor = '#F2F9F9ED';
             }
             item.style.backgroundColor = '#DEF3EE';
@@ -55,6 +56,8 @@ $(function () {
                 case "setting":
                     //TODO: url must change
                     // the data for setting page is static?!
+                    //Use modal like this
+                    showModal()
                     break;
                 default:
                     break;
@@ -64,26 +67,49 @@ $(function () {
         });
     });
 });
+const showModal = () => {
+    const modalEl = createModal('Error Message', 'Error message goes here.');
+
+    document.body.appendChild(modalEl);
+
+    $(modalEl).modal('show');
+
+    $(modalEl).on('hidden.bs.modal', () => {
+        document.body.removeChild(modalEl);
+    });
+}
+
+
 
 //routing
 $(window).on('hashchange load', function () {
-    document.getElementById("hidden-content").innerHTML=document.title ;
-    document.title=window.location.hash;
+    document.getElementById("hidden-content").innerHTML = document.title;
+    document.title = window.location.hash;
     const route = window.location.hash.replace('#', '') || 'home';
     $("#main-content").load(`${route}.html`);
 });
 
 // Word genarator
-function rWord(r){var t,n="bcdfghjklmnpqrstvwxyz",a="aeiou",e=function(r){return Math.floor(Math.random()*r)},o="";r=parseInt(r,10),n=n.split(""),a=a.split("");for(t=0;t<r/2;t++){var l=n[e(n.length)],p=a[e(a.length)];o+=0===t?l.toUpperCase():l,o+=2*t<r-1?p:""}return o}
+function rWord(r) {
+    var t, n = "bcdfghjklmnpqrstvwxyz", a = "aeiou", e = function (r) {
+        return Math.floor(Math.random() * r)
+    }, o = "";
+    r = parseInt(r, 10), n = n.split(""), a = a.split("");
+    for (t = 0; t < r / 2; t++) {
+        var l = n[e(n.length)], p = a[e(a.length)];
+        o += 0 === t ? l.toUpperCase() : l, o += 2 * t < r - 1 ? p : ""
+    }
+    return o
+}
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Generate a big table
-    for(var n=0;n<1000;n++){
+    for (var n = 0; n < 1000; n++) {
         var row = $("<tr>");
-        $("#sampleTableA").find("thead th").each(function() {
-            $("<td>",{
+        $("#sampleTableA").find("thead th").each(function () {
+            $("<td>", {
                 html: rWord(8),
-                style:"padding:2px;"
+                style: "padding:2px;"
             }).appendTo($(row));
         });
         row.appendTo($("#sampleTableA").find("tbody"));
@@ -92,9 +118,9 @@ $(document).ready(function() {
 
     // And make them fancy
     var fancyTableA = $("#sampleTableA").fancyTable({
-        sortColumn:0,
+        sortColumn: 0,
         pagination: true,
-        perPage:5,
-        globalSearch:true
+        perPage: 5,
+        globalSearch: true
     });
 });
