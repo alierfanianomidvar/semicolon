b937cc539a25fd62609f8b2e8cc3f4a45df4dd9c
Mohammad Torabi <mohammad.torabi@studenti.unipd.it>
2023-05-22 15:42:04 +0200

    Fixing login.html; Fixing login.js

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,58 @@
+// Define the accountUrls object
+const accountUrls = {
+    LOGIN: "your-login-url"
+};
+
+function sendData() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    // Validate email and password
+    if (!validateForm(email, password)) {
+        return;
+    }
+
+    const router = new Router();
+    return router.createFetch(accountUrls.LOGIN);
+}
+
+function validateForm(email, password) {
+    // Email validation regex pattern
+    const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    // Check if email and password are empty
+    if (email.trim() === "" || password.trim() === "") {
+        alert("Please enter both email and password.");
+        return false;
+    }
+
+    // Check if email is valid
+    if (!email.match(emailPattern)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+
+    // Check if password is at least 6 characters long
+    if (password.length < 6) {
+        alert("Password should be at least 6 characters long.");
+        return false;
+    }
+
+    // All validations passed
+    return true;
+}
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById("password");
+    const passwordIcon = document.getElementById("password-icon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        passwordIcon.classList.remove("bi-eye");
+        passwordIcon.classList.add("bi-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        passwordIcon.classList.remove("bi-eye-slash");
+        passwordIcon.classList.add("bi-eye");
+    }
+}
