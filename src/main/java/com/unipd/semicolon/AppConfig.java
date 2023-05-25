94bf34fa561f4dc17d3a75d6b53bdf6f6dea7e76
ali erfanianomidvar <alierfanianomidvar@alis-MacBook-Pro.local>
2023-05-25 12:04:10 +0200

    Add - reading the value of url, username and password of db from application.properties.

--- src/main/java/com/unipd/semicolon/AppConfig.java
+++ src/main/java/com/unipd/semicolon/AppConfig.java
@@ -1,21 +1,17 @@
 package com.unipd.semicolon;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.unipd.semicolon.business.constants.Constants;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
-@Configuration
 public class AppConfig {
-
-    @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/webApp");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("123456");
+        dataSource.setUrl(Constants.URL_DB);
+        dataSource.setUsername(Constants.USERNAME_DB);
+        dataSource.setPassword(Constants.PASSWORD_DB);
         return dataSource;
     }
 }
