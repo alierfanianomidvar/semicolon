a4903ef94fe8f9c4cb94ba29ea2922a98befeecc
hosseinhp <hossein.hosseinpour@studenti.unipd.it>
2023-06-01 12:00:57 +0200

    added some comments and use proper names and IDs

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -4,12 +4,10 @@ import {createTable, rWord} from "./table/table.js";
 
 $(() => {
 
-
-    console.log("Here");
-
     // Example usage: Create a table with dynamic column names and content
-    const tableId = "dynamicTable"; // Dynamic table ID
+    const tableId = "user_list"; // Dynamic table ID
     const columnNames = ["","Name", "Last Name", "Role", "Address", "Status"]; // Dynamic column names
+    //TODO: number of rows must be edited and we need to put the correct number of rows based on our user list fetch api
     const numRows = 1000; // Total number of rows
 
     // Generate the table ID dynamically
@@ -21,6 +19,7 @@ $(() => {
     // Append the table to the container
     $(".border").append(table);
 
+    // for passing the cellContentGenerator we need to define a proper function that returns the user actual information
     createTable(tableId, columnNames, numRows, () => {
         return rWord(8); // Generate random cell content
     }, "user");
