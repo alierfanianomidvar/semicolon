f10aebc1403746a528f8e796632ef26fd6ddf0b5
hosseinhp <hossein.hosseinpour@studenti.unipd.it>
2023-06-01 11:40:19 +0200

    refactored code for better performance to apply best practices

--- src/main/webapp/js/table/table.js
+++ src/main/webapp/js/table/table.js
@@ -1,6 +1,6 @@
 // Define a function for creating the table
 
-export const createTable = (tableId, columnNames, numRows, cellContentGenerator, type) => {
+export const createTable = (tableId, columnNames, numRows, cellContentGenerator, type = "default") => {
     // Generate the table headers
     const tableHeaders = $(`<thead>`).appendTo($(`#${tableId}`));
     const headerRow = $(`<tr>`).appendTo(tableHeaders);
@@ -10,25 +10,16 @@ export const createTable = (tableId, columnNames, numRows, cellContentGenerator,
 
     // Generate the table rows
     const tableBody = $(`<tbody>`).appendTo($(`#${tableId}`));
-    for (let n = 0; n < numRows; n++) {
-        const row = $("<tr>").appendTo(tableBody);
-
-        columnNames.forEach((columnName, index) => {
-            let cellContent = "";
-            if (index === 0 && type === "user") {
-                // Generate cell content for the "User" type table
-                cellContent = generateUserCellContent();
-            } else {
-                // Generate cell content for other types of tables using the provided cellContentGenerator function
-                cellContent = cellContentGenerator();
-            }
-
-            $("<td>", { html: cellContent, style: "padding:2px;" }).appendTo(row);
-        });
-
-        // Add the edit link button column
-        $("<td>", { html: "<a href='#'>Edit</a>", style: "padding:2px;" }).appendTo(row);
+    switch (type) {
+        case "user":
+            generateTableRows(numRows, tableBody, columnNames, "user", cellContentGenerator);
+            break;
+        default:
+            generateTableRows(numRows, tableBody, columnNames, "default", cellContentGenerator);
+            break;
     }
+
+
 
     // And make them fancy
     let fancyTableA = $(`#${tableId}`).fancyTable({
@@ -40,9 +31,9 @@ export const createTable = (tableId, columnNames, numRows, cellContentGenerator,
 };
 
 // Generate cell content for the "User" type table
-const generateUserCellContent = () => {
+const generateUserCellContent = (src) => {
     const image = $("<img>", {
-        src: "../images/users.svg", // Replace "path_to_round_image" with the actual path to the round image
+        src: src ? src : "../images/users.svg", // Replace "path_to_round_image" with the actual path to the round image
         class: "rounded-image", // Add a CSS class for styling if needed
         style: "width: 40px; height: 40px; margin-right: 20px; border: 1px solid lightgreen; border-radius: 50%;", // Adjust the width, height, and margin as desired
     });
@@ -55,7 +46,6 @@ const generateUserCellContent = () => {
     });
     cell.append(image);
     // cell.append(name);
-
     return cell;
 };
 
@@ -71,3 +61,24 @@ export const rWord = (r) => {
     }
     return o
 };
+
+
+function generateTableRows(numRows, tableBody, columnNames, type, cellContentGenerator) {
+    for (let n = 0; n < numRows; n++) {
+        const row = $("<tr>").appendTo(tableBody);
+
+        columnNames.forEach((columnName, index) => {
+            let cellContent = "";
+            if (type === "user" && index === 0) {
+                cellContent = generateUserCellContent();
+            } else {
+                cellContent = cellContentGenerator();
+            }
+            $("<td>", { html: cellContent, style: "padding:2px;" }).appendTo(row);
+        });
+
+        if (type === "user") {
+            $("<td>", { html: "<a href='#'>Edit</a>", style: "padding:2px;" }).appendTo(row);
+        }
+    }
+}
