42b83a30aa23f7be9c634eeaff3b0a0c786c4fcf
Irem Goksu Celik <iremgoksu.celik@studenti.unipd.it>
2023-06-01 14:28:29 +0200

    Update storageList.js

--- src/main/webapp/js/storageList.js
+++ src/main/webapp/js/storageList.js
@@ -1,4 +1,56 @@
 $(document).ready(function() {
+    // Add Product button
+    $('.add-btn button').click(function() {
+        // Get the values from the input fields
+        var name = $('#Name').val();
+        var type = $('#Type').val();
+        var price = $('#Price').val();
+        var quantity = $('#Quantity').val();
+        var threshold = $('#Threshold').val();
+
+        // Create a new row with the entered data
+        var newRow = '<tr>' +
+            '<td>' + name + '</td>' +
+            '<td>' + type + '</td>' +
+            '<td>' + price + '</td>' +
+            '<td>' + quantity + '</td>' +
+            '<td>' + threshold + '</td>' +
+            '</tr>';
+
+        // Append the new row to the table
+        $('#storage-data').append(newRow);
+
+        // Clear the input fields
+        $('#Name').val('');
+        $('#Type').val('');
+        $('#Price').val('');
+        $('#Quantity').val('');
+        $('#Threshold').val('');
+    });
+
+    // Filter button
+    $('.filter-btn button').click(function() {
+        // Get the selected filter values
+        var selectedThreshold = $('#FilterThreshold').val();
+        var selectedType = $('#FilterType').val();
+
+        // Show/hide rows based on the filter values
+        $('#storage-data tr').each(function() {
+            var rowThreshold = $(this).find('td:nth-child(5)').text();
+            var rowType = $(this).find('td:nth-child(2)').text();
+
+            if ((selectedThreshold === '' || rowThreshold === selectedThreshold) &&
+                (selectedType === '' || rowType === selectedType)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+});
+
+
+$(document).ready(function() {
     $('.search-box input[type="text"]').on('keyup', function() {
         const searchText = $(this).val().toLowerCase();
         $('table tbody tr').each(function() {
