15a6e78936ba403ba183e43711daee48e630d65a
hosseinhp <hossein.hosseinpour@studenti.unipd.it>
2023-06-01 09:51:57 +0200

    modified user js

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -1,23 +1,15 @@
-import {createTable} from "./table/table.js";
+import {createTable, rWord} from "./table/table.js";
+
+
 
 $(() => {
-    const rWord = (r) => {
-        let t, n = "bcdfghjklmnpqrstvwxyz", a = "aeiou", e = function (r) {
-            return Math.floor(Math.random() * r)
-        }, o = "";
-        r = parseInt(r, 10), n = n.split(""), a = a.split("");
-        for (t = 0; t < r / 2; t++) {
-            const l = n[e(n.length)], p = a[e(a.length)];
-            o += 0 === t ? l.toUpperCase() : l, o += 2 * t < r - 1 ? p : ""
-        }
-        return o
-    };
+
 
     console.log("Here");
 
     // Example usage: Create a table with dynamic column names and content
     const tableId = "dynamicTable"; // Dynamic table ID
-    const columnNames = ["Col A", "Col B", "Col C"]; // Dynamic column names
+    const columnNames = ["Col A", "Col B", "Col C", "Col D", "Col E"]; // Dynamic column names
     const numRows = 1000; // Total number of rows
 
     // Generate the table ID dynamically
@@ -27,17 +19,17 @@ $(() => {
     });
 
     // Append the table to the container
-    $(".container").append(table);
+    $(".border").append(table);
 
     createTable(tableId, columnNames, numRows, () => {
         return rWord(8); // Generate random cell content
-    });
+    }, "user");
 
     // And make the table fancy
-    const fancyTableA = $(`#${tableId}`).fancyTable({
-        sortColumn: 0,
-        pagination: true,
-        perPage: 5,
-        globalSearch: true,
-    });
+    // const fancyTableA = $(`#${tableId}`).fancyTable({
+    //     sortColumn: 0,
+    //     pagination: true,
+    //     perPage: 5,
+    //     globalSearch: true,
+    // });
 });
