4caaf2a2904354e627582feb2d9e2de09992ede7
Irem Goksu Celik <iremgoksu.celik@studenti.unipd.it>
2023-06-02 16:53:45 +0200

    Updating add button

--- src/main/webapp/js/storage.js
+++ src/main/webapp/js/storage.js
@@ -1,15 +1,15 @@
 import storageUrls from "./urls/storageUrls.js";
-import { createTable, rWord } from "./table/table.js";
+import {createTable, rWord} from "./table/table.js";
 
 $(document).ready(function() {
-    // Fetch storage data
     const ar = new Router();
     const data = ar.createFetch(storageUrls.GET_ALL);
     console.log(data);
 
     // Add Product button
-    $('.add-btn button').click(function() {
+    $('#add-button').click(function() {
         // Get the values from the input fields
+        console.log("")  // console.log("hello") for control
         var name = $('#Name').val();
         var type = $('#Type').val();
         var price = $('#Price').val();
@@ -37,54 +37,63 @@ $(document).ready(function() {
     });
 
     // Filter button
-    $('.filter-btn button').click(function() {
+    $('#filter-button').click(function() {
         // Get the selected filter values
-        var selectedThreshold = $('#FilterThreshold').val();
-        var selectedType = $('#FilterType').val();
+        var selectedThreshold = $('#Threshold').val();
+        var selectedType = $('#Type').val();
+        var selectedAmount = $('#Amount').val();
 
         // Show/hide rows based on the filter values
-        $('#storage-data tr').each(function() {
-            var rowThreshold = $(this).find('td:nth-child(5)').text();
-            var rowType = $(this).find('td:nth-child(2)').text();
+        $('#storage-data tbody tr').each(function() {
+            var rowThreshold = $(this).find('td:nth-child(6)').text();
+            var rowType = $(this).find('td:nth-child(3)').text();
+            var rowAmount = $(this).find('td:nth-child(4)').text();
 
             if ((selectedThreshold === '' || rowThreshold === selectedThreshold) &&
-                (selectedType === '' || rowType === selectedType)) {
+                (selectedType === '' || rowType === selectedType) &&
+                (selectedAmount === '' || rowAmount === selectedAmount)) {
                 $(this).show();
             } else {
                 $(this).hide();
             }
         });
     });
-});
 
-$(() => {
-    const tableId = "storage_list";
-    const columnNames = ["", "Name", "Type", "Price", "Quantity", "Threshold"];
-    const numRows = 1000;
+    $(() => {
+        const tableId = "storage_list"; // Dynamic table ID
+        const columnNames = ["", "Name", "Type", "Price", "Quantity", "Threshold"]; // Dynamic column names
+        //TODO: number of rows must be edited and we need to put the correct number of rows based on our user list fetch api
+        const numRows = 1000; // Total number of rows
 
-    const table = $(`<table>`, {
-        id: tableId,
-        class: "table table-striped sampleTable",
-    });
+        // Generate the table ID dynamically
+        const table = $(`<table>`, {
+            id: tableId,
+            class: "table table-striped sampleTable",
+        });
 
-    $(".border").append(table);
+        // Append the table to the container
+        $(".border").append(table);
 
-    createTable(tableId, columnNames, numRows, () => {
-        return rWord(8);
-    });
+        // for passing the cellContentGenerator we need to define a proper function that returns the user actual information
+        createTable(tableId, columnNames, numRows, () => {
+            return rWord(8); // Generate random cell content
+        });
 
-    $('.search-box input[type="text"]').on('keyup', function() {
-        const searchText = $(this).val().toLowerCase();
-        $('table tbody tr').each(function() {
-            const name = $(this).find('.name').text().toLowerCase();
-            const type = $(this).find('.type').text().toLowerCase();
-            const price = $(this).find('.price').text().toLowerCase();
-            const quantity = $(this).find('.quantity').text().toLowerCase();
-            const threshold = $(this).find('.threshold').text().toLowerCase();
+         /*And make the table fancy
+         const fancyTableA = $(`#${tableId}`).fancyTable({
+             sortColumn: 0,
+             pagination: true,
+             perPage: 5,
+             globalSearch: true,
+        });
+        */
 
-            const match = name.includes(searchText) || type.includes(searchText) || price.includes(searchText) || quantity.includes(searchText) || threshold.includes(searchText);
-
-            $(this).toggle(match);
+        $('.search-box input[type="text"]').on('keyup', function() {
+            const searchText = $(this).val().toLowerCase();
+            $('table tbody tr').each(function() {
+                const rowText = $(this).text().toLowerCase();
+                $(this).toggle(rowText.indexOf(searchText) > -1);
+            });
         });
     });
 });
