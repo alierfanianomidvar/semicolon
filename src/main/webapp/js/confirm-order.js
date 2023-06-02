e808357953f5d6cc27bc95737d5899508b3a8b50
Irem Goksu Celik <iremgoksu.celik@studenti.unipd.it>
2023-06-02 18:38:35 +0200

    Making connection with add-drug.html

--- src/main/webapp/js/confirm-order.js
+++ src/main/webapp/js/confirm-order.js
@@ -1,5 +1,4 @@
 function showPopup() {
-
     var popup = document.getElementById("popup");
     popup.style.display = "block";
 }
@@ -7,16 +6,16 @@ function showPopup() {
 function hidePopup() {
     var popup = document.getElementById("popup");
     popup.style.display = "none";
+    window.location.href = "add-drug.html";
 }
 
+
 function calculateTotal() {
-    var price = parseFloat(document.getElementById("price").value);
-    var tax = parseFloat(document.getElementById("tax").value);
-    var discount = parseFloat(document.getElementById("discount").value);
+    var price = parseFloat(document.getElementById("price").innerText);
+    var tax = parseFloat(document.getElementById("tax").innerText);
+    var discount = parseFloat(document.getElementById("discount").innerText);
 
     var total = price + (price * tax / 100) - (price * discount / 100);
 
-    document.getElementById("total").innerHTML = "Total Price: $" + total.toFixed(2);
+    document.getElementById("total-price").innerText = total.toFixed(2);
 }
-
-
