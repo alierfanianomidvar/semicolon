712e2056ddda1af8ad2fa4758a0f582f87930d47
hosseinhp <hossein.hosseinpour@studenti.unipd.it>
2023-06-02 20:40:43 +0200

    added an example of how to work with user table

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -4,11 +4,35 @@ import {createButtonsAndText, createTable, rWord} from "./table/table.js";
 
 $(() => {
 
+    //TODO: instead of this we need to have real data. This is just an example
+
+    const userData = [{
+        Name: "Ali",
+        LastName: "Mahdavi",
+        Role: "Admin",
+        Address: "via romana",
+        Status: "Active",
+        image: "../images/users.svg"
+    },{
+        Name: "Abi",
+        LastName: "mor",
+        Role: "Staff",
+        Address: "via romana",
+        Status: "Active",
+        image: "../images/users.svg"
+    },{
+        Name: "Alex",
+        LastName: "pegi",
+        Role: "Admin",
+        Address: "via romana",
+        Status: "Active",
+        image: "../images/users.svg"
+    }]
     // Example usage: Create a table with dynamic column names and content
     const tableId = "user_list"; // Dynamic table ID
-    const columnNames = ["","Name", "Last Name", "Role", "Address", "Status"]; // Dynamic column names
+    const columnNames = ["","Name", "LastName", "Role", "Address", "Status"]; // Dynamic column names
     //TODO: number of rows must be edited and we need to put the correct number of rows based on our user list fetch api
-    const numRows = 1000; // Total number of rows
+    const numRows = userData.length; // Total number of rows
 
     // Generate the table ID dynamically
     const table = $(`<table>`, {
@@ -20,9 +44,7 @@ $(() => {
     $(".border").append(table);
 
     // for passing the cellContentGenerator we need to define a proper function that returns the user actual information
-    createTable(tableId, columnNames, numRows, () => {
-        return rWord(8); // Generate random cell content
-    }, "user");
+    createTable(tableId, columnNames, numRows, userData, "user");
 
 
 
