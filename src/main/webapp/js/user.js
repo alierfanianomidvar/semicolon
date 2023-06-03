aeab9349c6c3f1f4d9695fb2c68d88efa47876f0
hamideh <haeri.hamideh@gmail.com>
2023-06-03 09:44:04 +0200

    اصلاح مشکل بک

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -1,36 +1,4 @@
-import {createTable, rWord} from "./table/table.js";
 import {showModal} from "./modal.js";
-
-
-
 $(() => {
-
     showModal()
-    // Example usage: Create a table with dynamic column names and content
-    const tableId = "user_list"; // Dynamic table ID
-    const columnNames = ["","Name", "Last Name", "Role", "Address", "Status"]; // Dynamic column names
-    //TODO: number of rows must be edited and we need to put the correct number of rows based on our user list fetch api
-    const numRows = 1000; // Total number of rows
-
-    // Generate the table ID dynamically
-    const table = $(`<table>`, {
-        id: tableId,
-        class: "table table-striped sampleTable",
-    });
-
-    // Append the table to the container
-    $(".border").append(table);
-
-    // for passing the cellContentGenerator we need to define a proper function that returns the user actual information
-    createTable(tableId, columnNames, numRows, () => {
-        return rWord(8); // Generate random cell content
-    }, "user");
-
-    // And make the table fancy
-    // const fancyTableA = $(`#${tableId}`).fancyTable({
-    //     sortColumn: 0,
-    //     pagination: true,
-    //     perPage: 5,
-    //     globalSearch: true,
-    // });
 });
