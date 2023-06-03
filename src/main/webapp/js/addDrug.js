05a2390c42ffda819d3c6c49b3ff2138f1315df1
Irem Goksu Celik <iremgoksu.celik@studenti.unipd.it>
2023-06-03 02:14:48 +0200

    Editing html and js

--- src/main/webapp/js/addDrug.js
+++ src/main/webapp/js/addDrug.js
@@ -1,4 +1,9 @@
+import orderUrls from "./urls/orderUrls";
+
 $(document).ready(function() {
+    const ar = new Router();
+    const data = ar.createFetch(orderUrls.GET_ALL);
+    console.log(data);
 
     $('#decrease-quantity').click(function() {
         let quantity = parseInt($('#quantity').val());
@@ -13,14 +18,32 @@ $(document).ready(function() {
         quantity++;
         $('#quantity').val(quantity);
     });
-});
 
-function showPopup() {
-    var popup = window.open("confirm-order.html", "_blank", "width=400,height=400");
-    popup.addEventListener("DOMContentLoaded", function () {
-        popup.document.getElementById("submit-order-btn").addEventListener("click", function () {
-            alert("Order submitted");
+    function showPopup() {
+        var popup = window.open("confirm-order.html", "_blank", "width=400,height=400");
+        popup.addEventListener("DOMContentLoaded", function () {
+            popup.document.getElementById("submit-order-btn").addEventListener("click", function () {
+                alert("Order submitted");
+            });
         });
-    });
-}
+    }
+
+    function calculateTotal() {
+        var total = 0;
+        var checkboxes = document.getElementsByClassName("order-checkbox");
+
+        for (var i = 0; i < checkboxes.length; i++) {
+            var checkbox = checkboxes[i];
+            if (checkbox.checked) {
+                var price = parseFloat(checkbox.getAttribute("data-price"));
+                total += price;
+            }
+        }
+        document.getElementById("total-price").textContent = "Total Price: $" + total.toFixed(2);
+    }
+
+    function resetTotal() {
+        document.getElementById("total-price").textContent = "$0";
+    }
+});
 
