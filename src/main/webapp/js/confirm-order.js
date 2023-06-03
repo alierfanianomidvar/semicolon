210a781325e3171468858ee5e710714805b27a95
Irem Goksu Celik <iremgoksu.celik@studenti.unipd.it>
2023-06-03 02:30:13 +0200

    Update confirm-order.js

--- src/main/webapp/js/confirm-order.js
+++ src/main/webapp/js/confirm-order.js
@@ -1,21 +1,44 @@
-function showPopup() {
-    var popup = document.getElementById("popup");
-    popup.style.display = "block";
-}
+import orderUrls from "./urls/orderUrls.js";
 
-function hidePopup() {
-    var popup = document.getElementById("popup");
-    popup.style.display = "none";
-    window.location.href = "add-drug.html";
-}
+$(document).ready(function() {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(orderUrls.GET_ALL);
+            const data = await response.json();
 
+            // Update the price, tax, and discount values in the HTML
+            document.getElementById("price").innerText = data.price;
+            document.getElementById("tax").innerText = data.tax;
+            document.getElementById("discount").innerText = data.discount;
 
-function calculateTotal() {
-    var price = parseFloat(document.getElementById("price").innerText);
-    var tax = parseFloat(document.getElementById("tax").innerText);
-    var discount = parseFloat(document.getElementById("discount").innerText);
+            calculateTotal(); // Calculate and update the total price
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-    var total = price + (price * tax / 100) - (price * discount / 100);
+    /*
+    function showPopup() {
+        var popup = document.getElementById("popup");
+        popup.style.display = "block";
+    }
+    */
 
-    document.getElementById("total-price").innerText = total.toFixed(2);
-}
+    function hidePopup() {
+        var popup = document.getElementById("popup");
+        popup.style.display = "none";
+        window.location.href = "add-drug.html";
+    }
+
+    function calculateTotal() {
+        var price = parseFloat(document.getElementById("price").innerText);
+        var tax = parseFloat(document.getElementById("tax").innerText);
+        var discount = parseFloat(document.getElementById("discount").innerText);
+
+        var total = price + (price * tax / 100) - (price * discount / 100);
+
+        document.getElementById("total-price").innerText = total.toFixed(2);
+    }
+
+    fetchData();
+});
