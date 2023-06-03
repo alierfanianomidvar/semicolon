865d78ffa3fc61617e99e64c94de6e890eecb32d
Irem Goksu Celik <iremgoksu.celik@studenti.unipd.it>
2023-06-03 22:53:55 +0200

    Editing for table Adding calculation

--- src/main/webapp/js/addDrug.js
+++ src/main/webapp/js/addDrug.js
@@ -1,49 +1,46 @@
 import orderUrls from "./urls/orderUrls";
+import {showModal} from "../js/modal.js";
+import {createGenericTable} from "./table/table.js";
+export const onInitial = () => {
 
-$(document).ready(function() {
-    const ar = new Router();
-    const data = ar.createFetch(orderUrls.GET_ALL);
-    console.log(data);
+    const orderData = {
+        price: "$13000",
+        tax: "10%",
+        discount: "12%"
+    };
+    showModal('Order', "Are you sure to submit your order?", 'Order_submit', orderData, "Total Price: 20$")
+    localStorage.getItem("getData")
+    createGenericTable("storage_list", ["","Name", "Price","IsActive","Quantity"]);
+};
 
-    $('#decrease-quantity').click(function() {
-        let quantity = parseInt($('#quantity').val());
-        if (quantity > 1) {
-            quantity--;
-            $('#quantity').val(quantity);
-        }
-    });
-
-    $('#increase-quantity').click(function() {
-        let quantity = parseInt($('#quantity').val());
-        quantity++;
-        $('#quantity').val(quantity);
-    });
-
-    function showPopup() {
+export const showPopup = () => {
         var popup = window.open("confirm-order.html", "_blank", "width=400,height=400");
         popup.addEventListener("DOMContentLoaded", function () {
             popup.document.getElementById("submit-order-btn").addEventListener("click", function () {
                 alert("Order submitted");
             });
         });
-    }
+}
 
-    function calculateTotal() {
-        var total = 0;
-        var checkboxes = document.getElementsByClassName("order-checkbox");
+export const calculateTotal = () => {
+    var total = 0;
+    var checkboxes = document.getElementsByClassName("order-checkbox");
 
-        for (var i = 0; i < checkboxes.length; i++) {
-            var checkbox = checkboxes[i];
-            if (checkbox.checked) {
-                var price = parseFloat(checkbox.getAttribute("data-price"));
-                total += price;
-            }
+    for (var i = 0; i < checkboxes.length; i++) {
+        var checkbox = checkboxes[i];
+        if (checkbox.checked) {
+            var price = parseFloat(checkbox.getAttribute("data-price"));
+            total += price;
         }
-        document.getElementById("total-price").textContent = "Total Price: $" + total.toFixed(2);
     }
+    document.getElementById("total-price").textContent = "Total Price: $" + total.toFixed(2);
+}
 
-    function resetTotal() {
-        document.getElementById("total-price").textContent = "$0";
-    }
-});
+export const resetTotal = () =>{
+    popup.addEventListener("DOMContentLoaded", function () {
+        popup.document.getElementById("cancel-btn").addEventListener("click", function () {
+            document.getElementById("total-price").textContent = "$0";
+        });
+    });
+}
 
