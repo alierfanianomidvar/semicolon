e4856e55fe14360a4d00933008044d25e6ff8118
hosseinhp <hossein.hosseinpour@studenti.unipd.it>
2023-06-03 14:07:03 +0200

    modified order modal to have title

--- src/main/webapp/js/modal.js
+++ src/main/webapp/js/modal.js
@@ -102,7 +102,8 @@ export const createModal = (title, content, type, data, footerContent) => {
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <h5 class="modal-title" id="errorModalLabel">${title}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
