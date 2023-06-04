c93fd3884cf1c94ff37c92e694c3fbef35f628ac
Ali Tahvildari <ali.tahvildari@studenti.unipd.it>
2023-06-04 16:59:00 +0200

    Fixing the bug add-product and remove add-supplier

--- src/main/webapp/js/addProduct.js
+++ src/main/webapp/js/addProduct.js
@@ -1,23 +1,6 @@
 import materialUrls from "./urls/materialUrls.js";
 import drugUrls from "./urls/drugUrls.js";
 
-const supplierListValue = document.getElementById("supplierId").value;
-const supplierListInt = parseInt(supplierListValue, 10);
-
-console.log(supplierListInt);
-
-const countryOfProductionSelect = document.getElementById("country-of-production");
-
-const countryCodes = [
-    "AF", "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS", "BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", "BG", "BF", "BI", "KH", "CM", "CA", "CV", "KY", "CF", "TD", "CL", "CN", "CX", "CC", "CO", "KM", "CG", "CD", "CK", "CR", "CI", "HR", "CU", "CW", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ", "ER", "EE", "ET", "FK", "FO", "FJ", "FI", "FR", "GF", "PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD", "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM", "VA", "HN", "HK", "HU", "IS", "IN", "ID", "IR", "IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE", "KI", "KP", "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MK", "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU", "NF", "MP", "NO", "OM", "PK", "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN", "PL", "PT", "PR", "QA", "RE", "RO", "RU", "RW", "BL", "SH", "KN", "LC", "MF", "PM", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SZ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH", "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "US", "UM", "UY", "UZ", "VU", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW"
-];
-
-countryCodes.forEach(countryCode => {
-    const option = document.createElement("option");
-    option.value = countryCode;
-    option.textContent = countryCode;
-    countryOfProductionSelect.appendChild(option);
-});
 
 export function sendData() {
     const productType = document.getElementById("product-type").value;
@@ -128,26 +111,6 @@ export const addProduct = async (event) => {
 export const cancelButton = () => {
     window.location.href = "supplier.html"; // Replace with the desired URL
 };
-$(function () {
-    console.log("A");
-    document.querySelector("#add-btn").addEventListener("click", addProduct);
-    document.querySelector("#cancel-btn").addEventListener("click",cancelButton);
-});
 
-$(document).ready(function() {
-    $('#product-type').change(function() {
-        const selectedOption = $(this).val();
-        if (selectedOption === 'MATERIAL') {
-            $('#sensitivity').parent().parent().hide();
-            $('#limitation').parent().parent().hide();
-            $('#need-prescription').parent().parent().hide();
-            $('#shape').parent().parent().hide();
-        } else {
-            $('#sensitivity').parent().parent().show();
-            $('#limitation').parent().parent().show();
-            $('#need-prescription').parent().parent().show();
-            $('#shape').parent().parent().show();
-        }
-    });
-});
+
 
