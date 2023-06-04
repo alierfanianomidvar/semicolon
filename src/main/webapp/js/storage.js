043ddbe07c770c4ee672728a88cfab8d23f2a865
Reihaneh <baghishani.r@gmail.com>
2023-06-04 22:52:43 +0200

    add storage list

--- src/main/webapp/js/storage.js
+++ src/main/webapp/js/storage.js
@@ -1,157 +1,57 @@
 import storageUrls from "./urls/storageUrls.js";
-import {showModal} from "../js/modal.js";
 import {createGenericTable} from "./table/table.js";
-export const onInitial = () => {
 
-    const orderData = {
-        price: "$13000",
-        tax: "10%",
-        discount: "12%"
-    };
-    //showModal('Order', "Are you sure to submit your order?", 'Order_submit', orderData, "Total Price: 20$")
-    localStorage.getItem("getData")
-    createGenericTable("storage_list", ["","Name", "Type", "Price", "Amount", "Threshold"]);
+let data;
+const router = new Router();
 
-    // const tableData = [
-    //     { id: 1, name: 'John Doe', age: 25, city: 'New York' },
-    //     { id: 2, name: 'Jane Smith', age: 30, city: 'London' },
-    //     { id: 3, name: 'Bob Johnson', age: 40, city: 'Paris' },
-    //     { id: 4, name: 'Alice Williams', age: 35, city: 'Tokyo' }
-    // ];
-    // showModal("Receipt", null, 'Receipt_submit', tableData, "Total Price: 20$")
-    // const orderData = {
-    //     price: "$13000",
-    //     tax: "10%",
-    //     discount: "12%"
-    // };
-    // showModal('Order', "Are you sure to submit your order?", 'Order_submit', orderData, "Total Price: 20$")
-    // createGenericTable("user_list", ["","Name", "Last Name", "Role", "Address", "Status"]);
-};
-
-export const createOption = async () => {
-    const router = new Router()
-    let storageData;
-    const storage = Promise.resolve(router.createFetch(storageUrls.GET_ALL))
-
-    storageData = await storage
-    console.log(storageData);
-
-    //for amount option
-    const selectedAmount = storageData.map(obj => obj.amount)
-    console.log(selectedAmount);
-
-    const selectElementAmount = document.getElementById('filterAmount');
-
-    selectedAmount.forEach(amount => {
-        const option = document.createElement('option');
-        option.text = amount;
-        selectElementAmount.add(option);
-    });
-
-    // for type option
-    const selectedType = storageData.map(obj => obj.type)
-    console.log(selectedType);
-
-    const selectElementType = document.getElementById('filterType');
-
-    selectedType.forEach(type => {
-        const option = document.createElement('option');
-        option.text = type;
-        selectElementType.add(option);
-    });
-
-    //for threshold option
-    const selectedThreshold = storageData.map(obj => obj.threshold)
-    console.log(selectedThreshold);
-
-    const selectElementThreshold = document.getElementById('filterThreshold');
-
-    selectedThreshold.forEach(threshold => {
-        const option = document.createElement('option');
-        option.text = threshold;
-        selectElementThreshold.add(option);
-    });
-}
-export const filtering = () => {
-    const filterButton = document.getElementById('filter-button');
-    filterButton.addEventListener('click', () => {
-        const storedData = JSON.parse(localStorage.getItem('getData')); // Retrieve stored data from localStorage
-        console.log(storedData);
-
-        // Filter the data
-        const selectedAmount = document.getElementById('filterAmount').value;
-        const selectedType = document.getElementById('filterType').value;
-        const selectedThreshold = document.getElementById('filterThreshold').value;
-
-        const filteredData = storedData.filter(obj => {
-            return (selectedAmount === '' || obj.amount === selectedAmount) &&
-                (selectedType === '' || obj.type === selectedType) &&
-                (selectedThreshold === '' || obj.threshold === selectedThreshold);
-        });
-
-        console.log(filteredData);
-
-        // Display the filtered data in the table
-        const tableBody = document.getElementById('tableBody');
-        tableBody.innerHTML = '';
-
-        if (filteredData.length === 0) {
-            const tr = document.createElement('tr');
-            const td = document.createElement('td');
-            td.textContent = 'No Products Found!';
-            td.className = 'text-center';
-            td.colSpan = 5;
-            tr.appendChild(td);
-            tableBody.appendChild(tr);
-        } else {
-            listTable(filteredData);
-        }
-    });
-};
-
-export const listTable = async () => {
-    const router = new Router()
-    let storageData;
-    const storage = Promise.resolve(router.createFetch(storageUrls.GET_ALL))
-
-    storageData = await storage
-    console.log(storageData);
-
-    const tableBody = document.getElementById('tableBody');
-
-    if (storageData.length === 0) {
-        const tr = document.createElement('tr');
-        const td = document.createElement('td');
-        td.textContent = 'No Products Found!';
-        td.className = 'text-center';
-        td.colSpan = 5;
-        tr.appendChild(td);
-        tableBody.appendChild(tr);
-    } else {
-        storageData.forEach((storage) => {
-            const tr = document.createElement('tr');
-
-            const nameTd = document.createElement('td');
-            nameTd.textContent = storage.name;
-            tr.appendChild(nameTd);
-
-            const typeTd = document.createElement('td');
-            typeTd.textContent = storage.type;
-            tr.appendChild(typeTd);
-
-            const priceTd = document.createElement('td');
-            priceTd.textContent = `$${storage.price}`;
-            tr.appendChild(priceTd);
-
-            const amountTd = document.createElement('td');
-            amountTd.textContent = storage.amount;
-            tr.appendChild(amountTd);
-
-            const thresholdTd = document.createElement('td');
-            thresholdTd.textContent = storage.threshold;
-            tr.appendChild(thresholdTd);
-
-            //tableBody.appendChild(tr);
-        });
+export const onInitial = async () => {
+    try {
+        data = await router.createFetch(storageUrls.GET_ALL);
+        console.log(data)
+        populateTable(data);
+    } catch (e) {
+        console.log("Error: ", e);
     }
+
 }
+
+// Populate table with data
+function populateTable(data) {
+
+    const tableData = data.map(obj => {
+        const isDrug = obj.drug !== null;
+        const newObj = {
+            Name: isDrug ? obj.drug.name : obj.material.name,
+            Type: isDrug ? "Drug" : "Material",
+            Amount: obj.amount,
+            Price: isDrug ? obj.drug.price : obj.material.price,
+            Threshold: isDrug ? obj.drug.limitation : obj.material.limitation
+        };
+        return newObj;
+    });
+    console.log(tableData)
+    createGenericTable(
+        "storage-list",
+        ["Name", "Type", "Price", "Amount", "Threshold"],
+        tableData,
+    );
+}
+
+const filterForm = document.getElementById('filterForm');
+filterForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Filter the data
+    const selectedType = document.getElementById('filterType').value;
+
+    const filteredData = data.filter(obj => {
+
+        return selectedType === '' || obj[selectedType] !== null;
+    });
+
+
+    const elem = document.getElementById('storage-list');
+    elem.remove();
+    populateTable(filteredData);
+
+});
