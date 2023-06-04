3d5fe7e4bfd124bc2c6832ddb9ffce3dd805f489
Irem Goksu Celik <iremgoksu.celik@studenti.unipd.it>
2023-06-04 23:49:16 +0200

    Changing js for table

--- src/main/webapp/js/addOrder.js
+++ src/main/webapp/js/addOrder.js
@@ -1,184 +1,61 @@
 import supplierUrls from "./urls/supplierUrls.js";
-
 import {showModal} from "../js/modal.js";
-import {createTable, rWord} from "./table/table.js";
 import {createGenericTable} from "./table/table.js";
-import drugUrls from "./urls/drugUrls.js";
-import materialUrls from "./urls/materialUrls.js";
+import orderUrls from "./urls/orderUrls";
 
-/*
-export const onInitial = () => {
+let data;
+const router = new Router();
 
-    const orderData = {
-        price: "$13000",
-        tax: "10%",
-        discount: "12%"
-    };
-
-    showModal('Order', "Are you sure to submit your order?", 'Order_submit', orderData, "Total Price: 20$")
-    localStorage.getItem("getData")
-    createGenericTable("order_list", ["","Name", "Price","IsActive","Quantity"]);
-};
-*/
-
-
-
-// Define the createTable function
-function createTable(tableId, columnNames, numRows, cellContentGenerator) {
-    const table = $(`#${tableId}`);
-
-    // Create the table header
-    const thead = $("<thead>");
-    const headerRow = $("<tr>");
-
-    columnNames.forEach((columnName) => {
-        const th = $("<th>", { text: columnName });
-        headerRow.append(th);
-    });
-
-    thead.append(headerRow);
-    table.append(thead);
-
-    // Create the table body with rows and cells
-    const tbody = $("<tbody>");
-
-    for (let i = 0; i < numRows; i++) {
-        const row = $("<tr>");
-
-        columnNames.forEach(() => {
-            const cell = $("<td>", {
-                text: cellContentGenerator(),
-            });
-            row.append(cell);
-        });
-        tbody.append(row);
+export const onInitial = async () => {
+    try {
+        data = await router.createFetch(orderUrls.GET_ALL);
+        console.log(data)
+        populateTable(data);
+    } catch (e) {
+        console.log("Error: ", e);
     }
-    table.append(tbody);
 }
 
-$(() => {
+// Populate table with data
+function populateTable(data) {
 
-    // Example usage: Create a table with dynamic column names and content
-    const tableId = "addOrder_list"; // Dynamic table ID
-    const columnNames = ["Name", "Price", "Is Active", "Quantity"]; // Dynamic column names
-    //TODO: number of rows must be edited and we need to put the correct number of rows based on our user list fetch api
-    const numRows = 1000; // Total number of rows
+    const tableData = data.map(obj => {
+        const isDrug = obj.drug !== null;
+        const newObj = {
+            Name: isDrug ? obj.drug.name : obj.material.name,
+            Price: isDrug ? obj.drug.price : obj.material.price,
+            IsActive: isDrug ? "Active" : "Not Active",
+            Amount: obj.amount
+        };
+        return newObj;
+    });
+    console.log(tableData)
+    createGenericTable(
+        "order-list",
+        ["Name", "Price", "IsActive", "Amount"],
+        tableData,
+    );
+}
 
-    // Generate the table ID dynamically
-    const table = $(`<table>`, {
-        id: tableId,
-        class: "table table-striped sampleTable",
+const filterForm = document.getElementById('filterForm');
+filterForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Filter the data
+    const selectedType = document.getElementById('filterType').value;
+
+    const filteredData = data.filter(obj => {
+
+        return selectedType === '' || obj[selectedType] !== null;
     });
 
-    // Append the table to the container
-    $(".border").append(table);
 
-    // for passing the cellContentGenerator we need to define a proper function that returns the user actual information
-    createTable(tableId, columnNames, numRows, () => {
-        return rWord(8); // Generate random cell content
-    });
+    const elem = document.getElementById('order-list');
+    elem.remove();
+    populateTable(filteredData);
 
-    // And make the table fancy
-    // const fancyTableA = $(`#${tableId}`).fancyTable({
-    //     sortColumn: 0,
-    //     pagination: true,
-    //     perPage: 5,
-    //     globalSearch: true,
-    // });
 });
 
-
-function generateFooterTextAndButtons(firstText, secondText, cancelButtonText, acceptButtonText) {
-    // Create the text element
-    const textElement = $("<div>", {
-        text: firstText,
-        css: {
-            fontWeight: "bold",
-            margin: "0.5rem", // Random margin between 1 and 10 pixels
-        },
-    });
-
-    // Create the cancel button
-    const cancelButton = $("<button>", {
-        text: cancelButtonText,
-        class: "btn btn-custom-secondary",
-        css: {
-            margin: "0.5rem", // Random margin between 1 and 10 pixels
-        },
-    });
-
-    // Create the accept button
-    const acceptButton = $("<button>", {
-        text: acceptButtonText,
-        class: "btn btn-custom",
-        data_toggle: "modal",
-        data_target: "showModal",
-        css: {
-            margin: "0.5rem", // Random margin between 1 and 10 pixels
-        },
-    });
-
-    // Create the container for the buttons
-    const buttonsContainer = $("<div>", {
-        class: "buttons-container",
-        css: {
-            display: "flex",
-            justifyContent: "flex-end",
-            width: "60%", // Adjust the width as needed
-        },
-    });
-
-    // Append the buttons to the container
-    buttonsContainer.append(cancelButton, acceptButton);
-
-    // Create the container for the text and buttons
-    const container = $("<div>", {
-        class: "bottom-container",
-        css: {
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginTop: "0.75rem", // Adjust the top margin as needed
-        },
-    });
-
-    // Append the text and buttons to the container
-    container.append(textElement, buttonsContainer);
-
-    // Append the container to the page or desired parent element
-    $(".border").append(container);
-}
-
-// Example usage
-generateFooterTextAndButtons("Total Price : $12000", "Second Text", "Cancel", "Submit Order");
-
-/*
-export const supplierOption = async () =>{
-    const data = JSON.parse(localStorage.getItem("getData"))
-    console.log(data)
-
-    const router = new Router()
-    let supplierData;
-    const supplier = Promise.resolve(router.createFetch(supplierUrls.GET_ALL))
-
-    supplierData = await supplier
-    console.log(supplierData);
-
-    //supplier option
-    const selectedSupplier = supplierData.map(obj => obj.name)
-    console.log(selectedSupplier);
-
-    const selectElementSupplier = document.getElementById('filterSupplier');
-
-    selectedSupplier.forEach(name =>{
-        const option = document.createElement('option');
-        option.text = name;
-        selectElementSupplier.add(option);
-    });
-}
- */
-
-/*
 export const supplierOption = async () =>{
     const router = new Router()
     let supplierData;
@@ -200,42 +77,9 @@ export const supplierOption = async () =>{
     });
 }
 
-export const sendOptionData = async () => {
-    const data = JSON.parse(localStorage.getItem("getData"))
-    console.log(data)
 
-    const router = new Router()
-    let materialData, drugData;
-
-    const material  = Promise.resolve(router.createFetch(materialUrls.GET_ALL));
-    materialData = await material;
-    console.log("m", materialData);
-
-    const drug  = Promise.resolve(router.createFetch(drugUrls.GET_ALL));
-    drugData = await drug;
-
-    return {
-        material: materialData,
-        drug: drugData
-    };
-}
-
-export async function optionData () {
-    let drug = [];
-    let material = [];
-    const data = await sendOptionData();
-    const drugMaterial = [...data.drug, ...data.material]
-    console.log("dm: ", drugMaterial)
-
-    const selectElement = document.getElementById('filterProduct');
-
-    drugMaterial.forEach(material => {
-        const option = document.createElement('option');
-        option.text = material.name;
-        selectElement.add(option);
-    });
-}
-
+/*
+//for calculation total price
 export const calculateTotal = () => {
     var total = 0;
     var checkboxes = document.getElementsByClassName("order-checkbox");
@@ -249,9 +93,9 @@ export const calculateTotal = () => {
     }
     document.getElementById("total-price").textContent = "Total Price: $" + total.toFixed(2);
 }
-*/
 
-/*
+
+//for making total 0 when clicking cancel button
 export const resetTotal = () => {
     $(document).ready(() => {
         const cancelButton = $('#cancel-btn');
@@ -266,7 +110,7 @@ export const resetTotal = () => {
     });
 };
 
-
+//link for confirm-order.html
 export const linking = () => {
     $(document).ready(() => {
         const submitButton = $('#submit-order-btn');
