b4c6d7489056f84e7ef8f2d2d97bb0c99311d5a4
Ali Tahvildari <ali.tahvildari@studenti.unipd.it>
2023-06-05 18:45:59 +0200

    Updating Table

--- src/main/webapp/js/addToStorage.js
+++ src/main/webapp/js/addToStorage.js
@@ -1,38 +1,57 @@
 // Get all drugs data
-import {createGenericTable} from "./table/table";
-import drugUrls from "./urls/drugUrls";
+import {createGenericTable} from "./table/table.js";
+import drugUrls from "./urls/drugUrls.js";
 
-let data;
+let data,drug,material;
 const router = new Router();
 
 export const onInitial = async () => {
     try {
-        data = await router.createFetch(drugUrls.GET_ALL);
-    console.log("MAMADjvkug",data)
-        populateTable(data);
+        drug = await router.createFetch(drugUrls.GET_ALL);
+        material = await router.createFetch(drugUrls.GET_ALL);
+
+        data = [
+            ...drug.map(item => ({ name: item.name, id : item.id ,type : "drug"})),
+            ...material.map(item => ({ name: item.name, id : item.id , type : "material"}))
+        ];
+        console.log("drug and material",data)
+        // populateTable(data);
+
+
+        const selectElement = document.getElementById("product-list");
+
+        data.forEach(item => {
+            const option = document.createElement("option");
+            option.text = item.name;
+            selectElement.add(option);
+        });
+
     } catch (e) {
         console.log("Error: ", e);
     }
 
 }
 
-// Populate table with data
-function populateTable(data) {
 
-    const tableData = data.map(obj => {
-        const newObj = {
-            Name: obj.name,
-            "Age Group": obj.ageGroup,
-            Sensivity: obj.sensitive ? "Sensitive" : "Not Sensitive",
-            Price: obj.price,
-            Limitation: obj.limitation
-        };
-        return newObj;
-    });
-    console.log(tableData)
-    createGenericTable(
-        "drug-list",
-        ["Name", "Age Group", "Sensivity", "Price", "Limitation"],
-        tableData,
-    );
-}
+
+// Populate table with data
+// function populateTable() {
+//
+//     const tableData = data.map(obj => {
+//         const newObj = {
+//             Name: obj.name,
+//             Amount: obj.amount,
+//             Threshold: obj.threshold,
+//             Discount: obj.discount,
+//         };
+//         return newObj;
+//     });
+//     console.log(tableData)
+//     createGenericTable(
+//         "storage-list",
+//         ["Name", "Amount", "Threshold", "Discount"],
+//         tableData,
+//         null,
+//         "default"
+//     );
+// }
