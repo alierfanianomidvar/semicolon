ebf62c5043cd106ece55ed231c9dbe7fcf54d9ae
hosseinhp <hossein.hosseinpour@studenti.unipd.it>
2023-06-05 09:39:46 +0200

    testing functionality

--- src/main/webapp/js/order_report.js
+++ src/main/webapp/js/order_report.js
@@ -1,7 +1,23 @@
 import {createGenericTable} from "./table/table.js";
+import orderUrls from "./urls/orderUrls.js";
 
+// const router = new Router();
 export const onInitial = () => {
-console.log("here");
+
+    // let orderData;
+    // try{
+    //     orderData = await router.createFetch(orderUrls.GET_ALL);
+    //     console.log("OrderData: ", orderData)
+    //     createGenericTable(
+    //         "order_report",
+    //         ["","id", "price", "status", "date"],
+    //         orderData,
+    //         null,
+    //         "default"
+    //     );
+    // }catch (e) {
+    //     console.log("Error: ", e);
+    // }
     const orderData = [{
         id:1,
         price: "$13000",
@@ -26,12 +42,13 @@ console.log("here");
 
     const footerContent = {
         button: {
-            active: true,
+            active: false,
             cancel: "Cancel",
             onCancel: () => {},
             submit: "Submit",
             onSubmit: () => {}
-        }
+        },
+
     }
 
     // showModal('Order', "Are you sure to submit your order?", 'Order_submit', orderData, "Total Price: 20$")
@@ -39,6 +56,8 @@ console.log("here");
         "order_report",
         ["","id", "price", "status", "date"],
         orderData,
+        null,
+        "default"
     );
 
 };
