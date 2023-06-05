acf761abc53a726d48c31c27d7c1a1b8f6342601
hosseinhp <hossein.hosseinpour@studenti.unipd.it>
2023-06-05 22:12:17 +0200

    modified order and receipt report

--- src/main/webapp/js/receipt_report.js
+++ src/main/webapp/js/receipt_report.js
@@ -24,26 +24,32 @@ export const onInitial = () => {
     // }]
 
     let receiptData = []
-    JSON.parse(localStorage.getItem("getData")).map((item, index) => {
-        const newObject = {
-            id: item.id,
-            paymentMethod: item.paymentMethod,
-            date: item.date.split("T")[0]
-        }
-        return receiptData.push(newObject)
-    })
+    if(localStorage.getItem("getData")) {
+        JSON.parse(localStorage.getItem("getData")).map((item, index) => {
+            const newObject = {
+                id: item.id,
+                paymentMethod: item.paymentMethod,
+                date: item.date.split("T")[0]
+            }
+            return receiptData.push(newObject)
+        })
+    }
+
 
     const footerContent = {
         text: {
-            active: true,
+            active: false,
             left: "Total: 5$",
             center: ""
+        },
+        button: {
+            active: false
         }
     }
 
     createGenericTable(
         "receipt_list",
-        ["","ID", "PaymentMethod", "Date"],
+        ["id", "PaymentMethod", "date"],
         receiptData,
         footerContent,
         'default'
