d7986518b3b1a934ee13e5d151513a0f7afe90e9
hosseinhp <hossein.hosseinpour@studenti.unipd.it>
2023-06-05 23:20:24 +0200

    fixed some bugs

--- src/main/webapp/js/receipt_report.js
+++ src/main/webapp/js/receipt_report.js
@@ -1,28 +1,6 @@
 import {createGenericTable} from "./table/table.js";
 export const onInitial = () => {
-
-    // const receiptData = [{
-    //     ID : 1,
-    //     Price: 32,
-    //     PaymentMethod: "CASH",
-    //     Date: "10-07-2023"
-    // },{
-    //     ID : 2,
-    //     Price: 2,
-    //     PaymentMethod: "CREDIT_CARD",
-    //     Date: "23-03-2023"
-    // },{
-    //     ID : 3,
-    //     Price: 55,
-    //     PaymentMethod: "DEBIT_CARD",
-    //     Date: "23-05-2023"
-    // },{
-    //     ID : 4,
-    //     Price: 169,
-    //     PaymentMethod: "PAYPAL",
-    //     Date: "05-12-2023"
-    // }]
-
+    
     let receiptData = []
     if(localStorage.getItem("getData")) {
         JSON.parse(localStorage.getItem("getData")).map((item, index) => {
