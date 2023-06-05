959762d93b5d4aa6aae291eb66bc75e65959458f
Mohammad Torabi <mohammad.torabi@studenti.unipd.it>
2023-06-05 18:53:41 +0200

    Testing order_report.js; receipt_report.js

--- src/main/webapp/js/order_report.js
+++ src/main/webapp/js/order_report.js
@@ -1,16 +1,27 @@
 import {createGenericTable} from "./table/table.js";
 export const onInitial = () => {
 
-    const orderData = [{
-        ID:1 , Price:12 , Status:"DELIVERED" , Date:"12-01-2023"
-    },{
-        ID:2 , Price:32 , Status:"PENDING" , Date:"05-04-2023"
-    },{
-        ID:3 , Price:6 , Status:"DELIVERED" , Date:"30-11-2023"
-    },{
-        ID:4 , Price:89 , Status:"CANCELLED" , Date:"11-09-2023"
-    }]
+    // const orderData = [{
+    //     ID:1 , Price:12 , Status:"DELIVERED" , Date:"12-01-2023"
+    // },{
+    //     ID:2 , Price:32 , Status:"PENDING" , Date:"05-04-2023"
+    // },{
+    //     ID:3 , Price:6 , Status:"DELIVERED" , Date:"30-11-2023"
+    // },{
+    //     ID:4 , Price:89 , Status:"CANCELLED" , Date:"11-09-2023"
+    // }]
 
+    let orderData = []
+    JSON.parse(localStorage.getItem("getData")).map((item, index) => {
+        const newObject = {
+            id: item.id,
+            price: item.price,
+            status:item.status,
+            date: item.date.split("T")[0]
+        }
+        return orderData.push(newObject)
+    })
+    console.log("Hamide analyzer",orderData);
     const footerContent = {
         text: {
             active: true,
--- src/main/webapp/js/receipt_report.js
+++ src/main/webapp/js/receipt_report.js
@@ -1,27 +1,37 @@
 import {createGenericTable} from "./table/table.js";
 export const onInitial = () => {
 
-    const receiptData = [{
-        ID : 1,
-        Price: 32,
-        PaymentMethod: "CASH",
-        Date: "10-07-2023"
-    },{
-        ID : 2,
-        Price: 2,
-        PaymentMethod: "CREDIT_CARD",
-        Date: "23-03-2023"
-    },{
-        ID : 3,
-        Price: 55,
-        PaymentMethod: "DEBIT_CARD",
-        Date: "23-05-2023"
-    },{
-        ID : 4,
-        Price: 169,
-        PaymentMethod: "PAYPAL",
-        Date: "05-12-2023"
-    }]
+    // const receiptData = [{
+    //     ID : 1,
+    //     Price: 32,
+    //     PaymentMethod: "CASH",
+    //     Date: "10-07-2023"
+    // },{
+    //     ID : 2,
+    //     Price: 2,
+    //     PaymentMethod: "CREDIT_CARD",
+    //     Date: "23-03-2023"
+    // },{
+    //     ID : 3,
+    //     Price: 55,
+    //     PaymentMethod: "DEBIT_CARD",
+    //     Date: "23-05-2023"
+    // },{
+    //     ID : 4,
+    //     Price: 169,
+    //     PaymentMethod: "PAYPAL",
+    //     Date: "05-12-2023"
+    // }]
+
+    let receiptData = []
+    JSON.parse(localStorage.getItem("getData")).map((item, index) => {
+        const newObject = {
+            id: item.id,
+            paymentMethod: item.paymentMethod,
+            date: item.date.split("T")[0]
+        }
+        return receiptData.push(newObject)
+    })
 
     const footerContent = {
         text: {
@@ -33,7 +43,7 @@ export const onInitial = () => {
 
     createGenericTable(
         "receipt_list",
-        ["","ID", "Price", "PaymentMethod", "Date"],
+        ["","ID", "PaymentMethod", "Date"],
         receiptData,
         footerContent,
         'default'
